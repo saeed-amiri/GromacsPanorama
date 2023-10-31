d6a1cde9ea0b53ceaef55b8e4001dde2f4375c2e
Saeed Amiri <samiri@uni-bremen.de>
2023-10-31 15:01:51 +0100

    reading xvg headers

---
+++ src/common/xvg_to_dataframe.py
@@ -0,0 +1,74 @@
+"""reading xvg files and return it as ddaframe"""
+
+import sys
+import pandas as pd
+
+from common import logger
+from common import my_tools
+from common.colors_text import TextColor as bcolors
+
+
+class XvgParser:
+    """reading and getting info from xvg file"""
+
+    info_msg: str = 'Message from XvgParser:\n'  # Meesage in methods to log
+
+    fname: str  # Name of the input file
+    title: str = ''  # Title of the data
+    xaxis: str = ''  # Label of the x axis
+    yaxis: str = ''  # Label of the y axis
+
+    def __init__(self,
+                 fname: str,  # Name of the xvg file
+                 log: logger.logging.Logger
+                 ) -> None:
+        self.fname = fname
+
+        self.get_xvg()
+        self.write_log_msg(log)
+
+    def get_xvg(self) -> pd.DataFrame:
+        """parse xvg file"""
+        with open(self.fname, 'r', encoding='utf8') as f_r:
+            while True:
+                line: str = f_r.readline().strip()
+                if line.startswith('#'):
+                    pass
+                elif line.startswith('@'):
+                    self.parse_header(line)
+                if not line:
+                    break
+
+    def parse_header(self,
+                     line: str  # Read line from the file
+                     ) -> None:
+        """get info from the header with @"""
+        line = my_tools.drop_string(line, '@').strip()
+        if line.startswith('title'):
+            self.title = self.clean_line(line, ['title'])
+        elif line.startswith('xaxis'):
+            self.xaxis = self.clean_line(line, ['xaxis', 'label'])
+        elif line.startswith('yaxis'):
+            self.yaxis = self.clean_line(line, ['yaxis', 'label'])
+
+    @staticmethod
+    def clean_line(line: str,  # line to clean
+                   droplets: list[str]  # Strings to drop from the line
+                   ) -> str:
+        """clean up the line"""
+        tmp_str: str = line
+        for droplet in droplets:
+            tmp_str = my_tools.drop_string(tmp_str, droplet)
+        return my_tools.extract_string(tmp_str)[0]
+
+    def write_log_msg(self,
+                      log: logger.logging.Logger  # Name of the output file
+                      ) -> None:
+        """writing and logging messages from methods"""
+        log.info(self.info_msg)
+        print(f'{bcolors.OKBLUE}{self.__module__}:\n'
+              f'\t{self.info_msg}\n{bcolors.ENDC}')
+
+
+if __name__ == "__main__":
+    XvgParser(sys.argv[1], log=logger.setup_logger(log_name='xvg.log'))

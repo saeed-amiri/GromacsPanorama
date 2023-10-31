89d1b4e04c4ca3d29c614d35b6bbc6a9144dbc9c
Saeed Amiri <samiri@uni-bremen.de>
2023-10-31 07:33:47 +0100

    try import

--- tests/common/test_logger.py
+++ tests/common/test_logger.py
@@ -1,6 +1,6 @@
 # tests/common/test_logger.py
 
-from common.logger import check_log_file
+from src.common.logger import check_log_file
 
 def test_check_log_file():
     assert check_log_file("test_log") == "test_log.1"

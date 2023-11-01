a3a042d8d04ae8dfddaaa5488e2fddbdb2dad109
Saeed Amiri <samiri@uni-bremen.de>
2023-11-01 12:16:19 +0100

    select samples

--- src/module2_statistics/bootstrap_sampler.py
+++ src/module2_statistics/bootstrap_sampler.py
@@ -6,7 +6,9 @@ any statistic, making it a flexible method for uncertainty estimation
 """
 
 import sys
-import common.logger as logger
+import random
+import numpy as np
+from common import logger
 import common.xvg_to_dataframe as xvg_to_df
 from common.colors_text import TextColor as bcolors
 
@@ -15,13 +17,30 @@ class BootStrping:
     """do the boostraping sampling and error propagation"""
 
     info_msg: str = 'Message from BootStrping:\n'  # Meesage in methods to log
-    xvg: "xvg_to_df"  # Parsed datafile
+    xvg: "xvg_to_df.XvgParser"  # Parsed datafile
+
+    booter: str = 'Surf_SurfTen'  # Name of the columns to bootstrap them
 
     def __init__(self,
                  fname: str,  # Data file xvg format
                  log: logger.logging.Logger
                  ) -> None:
         self.xvg = xvg_to_df.XvgParser(fname=fname, log=log)
+        self.initiate(log)
+
+    def initiate(self,
+                 log: logger.logging.Logger
+                 ) -> None:
+        """do the sampling here"""
+        samples: list[list[np.float64]] = self.random_replacement()
+
+    def random_replacement(self) -> list[list[np.float64]]:
+        """Randomly Select With Replacement"""
+        samples: list[list[np.float64]] = []
+        for _ in range(self.xvg.nr_frames):
+            samples.append(random.choices(self.xvg.xvg_df[self.booter],
+                                          k=self.xvg.nr_frames))
+        return samples
 
 
 if __name__ == "__main__":

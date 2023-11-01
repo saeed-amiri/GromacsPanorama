a9e6d414baa29f16c4f3dfef59b66862be010f6f
Saeed Amiri <samiri@uni-bremen.de>
2023-11-01 14:11:04 +0100

    logging

--- src/module2_statistics/bootstrap_sampler.py
+++ src/module2_statistics/bootstrap_sampler.py
@@ -6,6 +6,7 @@ any statistic, making it a flexible method for uncertainty estimation
 """
 
 import sys
+import json
 import random
 import typing
 from collections import Counter
@@ -18,23 +19,23 @@ from common.colors_text import TextColor as bcolors
 class BootStrping:
     """do the boostraping sampling and error propagation"""
 
-    stats_dic: dict[str, typing.Any] = {}
-
     info_msg: str = 'Message from BootStrping:\n'  # Meesage in methods to log
-    xvg: "xvg_to_df.XvgParser"  # Parsed datafile
 
+    xvg: "xvg_to_df.XvgParser"  # Parsed datafile
     booter: str = 'Surf_SurfTen'  # Name of the columns to bootstrap them
+    stats_dict: dict[str, typing.Any] = {}
 
     def __init__(self,
                  fname: str,  # Data file xvg format
                  log: logger.logging.Logger
                  ) -> None:
         self.xvg = xvg_to_df.XvgParser(fname=fname, log=log)
-        self.initiate_normal(log)
+        self.info_msg += \
+            f'\tBootstraping for `{fname}` on column `{self.booter}`\n'
+        self.initiate_normal()
+        self.write_log_msg(log)
 
-    def initiate_normal(self,
-                        log: logger.logging.Logger
-                        ) -> None:
+    def initiate_normal(self) -> None:
         """do the sampling here"""
         samples: list[np.float64] = self.random_replacement()
         self.analysis_sample(samples)
@@ -53,10 +54,11 @@ class BootStrping:
                         ) -> None:
         """calculate std and averages"""
         sample_arr: np.ndarray = np.asarray(samples)
-        self.stats_dic['std'] = np.std(sample_arr)
-        self.stats_dic['mean'] = np.mean(sample_arr)
-        self.stats_dic['mode'] = \
-            self.calc_mode(sample_arr, self.stats_dic['std']/5)
+        self.stats_dict['std'] = np.std(sample_arr)
+        self.stats_dict['mean'] = np.mean(sample_arr)
+        self.stats_dict['mode'] = \
+            self.calc_mode(sample_arr, self.stats_dict['std']/5)
+        self.info_msg += f'\tstats:{json.dumps(self.stats_dict, indent=8)}'
 
     @staticmethod
     def calc_mode(samples: np.ndarray,
@@ -71,6 +73,14 @@ class BootStrping:
         modes \
             = [value for value, count in counts.items() if count == max_count]
         return modes[0]
+
+    def write_log_msg(self,
+                      log: logger.logging.Logger  # Name of the output file
+                      ) -> None:
+        """writing and logging messages from methods"""
+        log.info(self.info_msg)
+        print(f'{bcolors.OKBLUE}{self.__module__}:\n'
+              f'\t{self.info_msg}\n{bcolors.ENDC}')
 
 
 if __name__ == "__main__":

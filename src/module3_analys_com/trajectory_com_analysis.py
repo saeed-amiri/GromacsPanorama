cc1972202f7110faeb36f5aee17b9bc5d66a7725
Saeed Amiri <samiri@uni-bremen.de>
2023-11-02 14:28:00 +0100

    get com

---
+++ src/module3_analys_com/trajectory_com_analysis.py
@@ -0,0 +1,18 @@
+"""
+Read the com file wrote by trajectory_to_com_dataframe
+read the com and return split them into np.ndarray into dict
+
+"""
+
+import sys
+import typing
+import numpy as np
+import pandas as pd
+from common import logger
+import common.xvg_to_dataframe as xvg_to_df
+from common.colors_text import TextColor as bcolors
+
+
+
+if __name__ == "__main__":
+    pass

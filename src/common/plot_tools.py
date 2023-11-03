35559565e5f1b0385d2909949424ae7eb19c452d
Saeed Amiri <samiri@uni-bremen.de>
2023-11-03 12:13:12 +0100

    update ratio

--- src/common/plot_tools.py
+++ src/common/plot_tools.py
@@ -27,7 +27,7 @@ def set_sizes(width: float,  # Width of the plot in points
     """
     fig_width_pt = width*fraction
     inches_per_pt = 1/72.27
-    golden_ratio = (5**0.5 - 1)/2
+    golden_ratio = (5**0.5 - 1)/3
     fig_width_in = fig_width_pt * inches_per_pt
     fig_height_in = fig_width_in * golden_ratio
     fig_dim = (fig_width_in, fig_height_in)

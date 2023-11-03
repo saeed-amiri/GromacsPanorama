fb61498ef63eab23ff1061c5fdbe2637d906b022
Saeed Amiri <samiri@uni-bremen.de>
2023-11-03 12:16:22 +0100

    plot and save to xyz file

--- src/module3_analysis_com/com_plotter.py
+++ src/module3_analysis_com/com_plotter.py
@@ -11,6 +11,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from common import logger
 from common import plot_tools
+from common import static_info as stinfo
 from common.colors_text import TextColor as bcolors
 
 
@@ -26,11 +27,14 @@ class ComPlotter:
                  to_png: bool = False,  # If want to save as png
                  out_suffix: str = 'com.png',  # Suffix for fout, if to_png
                  to_xyz: bool = False,  # If want to save the traj to xyz file
-                 xyz_file: str = 'com.xyz'  # name of the png file, if to_xyz
+                 xyz_suufix: str = 'com.xyz',  # Suffix to xyz file, if to_xyz
+                 atom: str = "O"
                  ) -> None:
-        self.info_msg += f'mkaing graph for frame: `{index}`'
+        self.info_msg += f'\tmkaing graph for frame: `{index}`\n'
         xyz_arr: np.ndarray = self._parse_arr(com_arr, index)
-        self.make_graph(xyz_arr, to_png, out_suffix)
+        self.make_graph(xyz_arr, index, to_png, out_suffix)
+        if to_xyz:
+            self.write_xyz_file(xyz_arr, index, atom, xyz_suufix)
         self._write_msg(log)
 
     def _parse_arr(self,
@@ -42,16 +46,45 @@ class ComPlotter:
 
     def make_graph(self,
                    xyz_arr: np.ndarray,
+                   index: int,
                    to_png: bool = False,  # If want to save as png
                    out_suffix: str = 'com.png',  # Suffix for fout,if to_png
                    ) -> None:
         """plot and, if, save it"""
-        fig_i, axes = \
-            plot_tools.mk_canvas(x_range=(0, 240), ncols=3)
-        axes[0].scatter(xyz_arr[:, 0], xyz_arr[:, 1])
-        axes[1].scatter(xyz_arr[:, 0], xyz_arr[:, 2])
-        axes[2].scatter(xyz_arr[:, 1], xyz_arr[:, 2])
-        plt.show()
+        fig_i, axes = plot_tools.mk_canvas(
+            x_range=(0, 240), ncols=3, width=stinfo.plot['width']*4)
+
+        labels = ['xy', 'xz', 'yz']
+        data = [xyz_arr[:, [0, 1]], xyz_arr[:, [0, 2]], xyz_arr[:, [1, 2]]]
+
+        for i, ax_i in enumerate(axes):
+            ax_i.scatter(
+                data[i][:, 0],
+                data[i][:, 1],
+                label=f'{labels[i]},frame:{index}')
+            ax_i.set_xlabel('x [nm]' if i != 2 else 'y [nm]')
+            ax_i.set_ylabel('y [nm]' if i == 0 else 'z [nm]')
+
+        if to_png:
+            plot_tools.save_close_fig(
+                fig_i, axes, fname := f"{index}_{out_suffix}")
+            self.info_msg += f'\tThe out picture is: {fname}\n'
+        else:
+            plt.show()
+
+    def write_xyz_file(self,
+                       xyz_arr: np.ndarray,
+                       index: int,
+                       atom: str,
+                       xyz_suffix: str,
+                       ) -> None:
+        """Write xyz_arr to an XYZ format file."""
+        with open(f'{index}_{xyz_suffix}', 'w', encoding='utf8') as f_r:
+            num_atoms, _ = xyz_arr.shape
+            f_r.write(f"{num_atoms}\n\n")
+            for i in range(num_atoms):
+                f_r.write(f"{atom} "
+                          f"{xyz_arr[i, 0]} {xyz_arr[i, 1]} {xyz_arr[i, 2]}\n")
 
     def _write_msg(self,
                    log: logger.logging.Logger  # To log

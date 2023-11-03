c8e0fe52d08f53acd8eb7bd2613a82a307e56e44
Saeed Amiri <samiri@uni-bremen.de>
2023-11-03 10:25:38 +0100

    make canvas and subplots

--- src/module3_analysis_com/com_plotter.py
+++ src/module3_analysis_com/com_plotter.py
@@ -8,22 +8,30 @@ the array is in the format of the com:
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 from common import logger
+from common import plot_tools
+from common.colors_text import TextColor as bcolors
 
 
 class ComPlotter:
     """plot the center of mass"""
+
+    info_msg: str = 'Messages from ComPlotter:\n'  # To log
 
     def __init__(self,
                  com_arr: np.ndarray,  # The array to plot
                  index: int,  # Time frame to plot
                  log: logger.logging.Logger,
                  to_png: bool = False,  # If want to save as png
-                 out_file: str = 'com.png',  # name of the png file, if to_png
+                 out_suffix: str = 'com.png',  # Suffix for fout, if to_png
                  to_xyz: bool = False,  # If want to save the traj to xyz file
                  xyz_file: str = 'com.xyz'  # name of the png file, if to_xyz
                  ) -> None:
-        xyz_zrr: np.ndarray = self._parse_arr(com_arr, index)
+        self.info_msg += f'mkaing graph for frame: `{index}`'
+        xyz_arr: np.ndarray = self._parse_arr(com_arr, index)
+        self.make_graph(xyz_arr, to_png, out_suffix)
+        self._write_msg(log)
 
     def _parse_arr(self,
                    com_arr: np.ndarray,  # The array to plot
@@ -31,6 +39,27 @@ class ComPlotter:
                    ) -> np.ndarray:
         """return the data for plotting in one array"""
         return com_arr[index, :].reshape(-1, 3)
+
+    def make_graph(self,
+                   xyz_arr: np.ndarray,
+                   to_png: bool = False,  # If want to save as png
+                   out_suffix: str = 'com.png',  # Suffix for fout,if to_png
+                   ) -> None:
+        """plot and, if, save it"""
+        fig_i, axes = \
+            plot_tools.mk_canvas(x_range=(0, 240), ncols=3)
+        axes[0].scatter(xyz_arr[:, 0], xyz_arr[:, 1])
+        axes[1].scatter(xyz_arr[:, 0], xyz_arr[:, 2])
+        axes[2].scatter(xyz_arr[:, 1], xyz_arr[:, 2])
+        plt.show()
+
+    def _write_msg(self,
+                   log: logger.logging.Logger  # To log
+                   ) -> None:
+        """write and log messages"""
+        print(f'{bcolors.OKCYAN}{ComPlotter.__name__}:\n'
+              f'\t{self.info_msg}{bcolors.ENDC}')
+        log.info(self.info_msg)
 
 
 if __name__ == '__mian__':

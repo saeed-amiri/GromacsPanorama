66adf91f29b085f4fe5dc604a5dadaa623c354a3
Saeed Amiri <samiri@uni-bremen.de>
2023-11-03 07:35:00 +0100

    initial analysing aqua phase

--- src/module3_analysis_com/aquatic_analysis.py
+++ src/module3_analysis_com/aquatic_analysis.py
@@ -5,9 +5,33 @@ reduction of surface from nanoparticle.
 """
 
 import typing
+import pandas as pd
 
 from common import logger
-from module3_analys_com.com_file_parser import GetCom
+from module3_analysis_com.com_file_parser import GetCom
+
+
+class GetSurface:
+    """find the surface of the water"""
+    
+    info_msg: str = 'Message from GetSurface:\n'  # Meesage in methods to log
+
+    def __init__(self,
+                 water_df: pd.DataFrame,
+                 log: logger.logging.Logger
+                 ) -> None:
+        self.get_water_surface(water_df, log)
+
+    def get_water_surface(self,
+                 water_df: pd.DataFrame,
+                 log: logger.logging.Logger
+                 ) -> None:
+        """
+        mesh the box and find resides with highest z value in them
+        """
+        print(water_df)
+        
+
 
 class AnalysisAqua:
     """get everything from water!"""
@@ -15,7 +39,8 @@ class AnalysisAqua:
                  parsed_com: "GetCom",
                  log: logger.logging.Logger
                  ) -> None:
-        print(parsed_com.__dict__)
+        GetSurface(parsed_com.split_arr_dict['SOL'], log)
+
 
 if __name__ == "__main__":
     AnalysisAqua(GetCom(), log=logger.setup_logger("aqua_log"))

44f1681e1c8618f36b1cffc8b9afd286d0327694
Saeed Amiri <samiri@uni-bremen.de>
2023-11-04 14:58:34 +0100

    plot thg surface

--- src/module3_analysis_com/surface_plotter.py
+++ src/module3_analysis_com/surface_plotter.py
@@ -3,7 +3,9 @@ plot surface water, in different styles
 """
 
 import random
+import typing
 import numpy as np
+import matplotlib.pylab as plt
 
 from common import logger
 from common import plot_tools
@@ -22,9 +24,9 @@ class SurfPlotter:
     def __init__(self,
                  surf_dict: dict[int, np.ndarray],
                  log: logger.logging.Logger,
-                 indices: list[int] = [],
+                 indices: typing.Optional[list[int]] = None,
                  fout_suffix: str = 'surface.png',
-                 nr_fout: int = 5  # Numbers pics incase the list is empty
+                 nr_fout: int = 5  # Numbers pics in case the list is empty
                  ) -> None:
         selected_frames: dict[int, np.ndarray] = \
             self.get_selected_frames(surf_dict, indices, nr_fout)
@@ -33,22 +35,31 @@ class SurfPlotter:
 
     def get_selected_frames(self,
                             surf_dict: dict[int, np.ndarray],
-                            indices: list[int],
+                            indices: typing.Optional[list[int]],
                             nr_fout: int
                             ) -> dict[int, np.ndarray]:
         """return the numbers of the wanted frames data"""
-        if not indices:
+        if indices is None:
             indices = random.sample(list(surf_dict.keys()), nr_fout)
         self.info_msg += f'\tThe selected indices are:\n\t\t{indices}\n'
         return {key: surf_dict[key] for key in indices}
-        
 
     def plot_surface(self,
                      selected_frames: dict[int, np.ndarray],
                      fout_suffix: str
                      ) -> None:
         """plot the surface"""
-        print(selected_frames)
+        for frame, value in selected_frames.items():
+            fig_i, ax_i = plot_tools.mk_canvas((0, 240))
+            scatter = ax_i.scatter(value[:, 0], value[:, 1], c=value[:, 2],
+                                   s=15, label=f'frame: {frame}')
+            plt.axis('equal')
+            cbar = plt.colorbar(scatter)
+            cbar.set_label('Z-Coordinate [A]')
+            ax_i.set_xlabel('X_Coordinate [A]')
+            ax_i.set_ylabel('Y_Coordinate [A]')
+            plot_tools.save_close_fig(
+                fig_i, ax_i, fname=f'{frame}_{fout_suffix}', legend=False)
 
     def _write_msg(self,
                    log: logger.logging.Logger  # To log
@@ -56,7 +67,7 @@ class SurfPlotter:
         """write and log messages"""
         print(f'{bcolors.OKCYAN}{SurfPlotter.__name__}:\n'
               f'\t{self.info_msg}{bcolors.ENDC}')
-        log.info(self.info_msg)        
+        log.info(self.info_msg)
 
 
 if __name__ == "__main__":

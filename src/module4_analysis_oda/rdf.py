e8bfc3e7bb92d469d4e26d483ef25eec3adc6ece
Saeed Amiri <samiri@uni-bremen.de>
2023-11-08 08:00:59 +0100

    initiate rdf

---
+++ src/module4_analysis_oda/rdf.py
@@ -0,0 +1,17 @@
+"""
+calculating rdf from nanoparticle segment center.
+The nanoparticle com for this situation (x, y, z_interface)
+The z valu is only used for droping the oda which are not at interface
+"""
+
+import sys
+import typing
+from common import logger
+from common.colors_text import TextColor as bcolors
+
+if typing.TYPE_CHECKING:
+    from common.com_file_parser import GetCom
+
+
+if __name__ == '__main__':
+    pass

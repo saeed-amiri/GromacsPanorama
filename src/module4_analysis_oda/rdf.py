69478b602ccbf3f6640fe4de759b943a146d26bd
Saeed Amiri <samiri@uni-bremen.de>
2023-11-08 09:52:58 +0100

    get oda-np distances with applying pbc

--- src/module4_analysis_oda/rdf.py
+++ src/module4_analysis_oda/rdf.py
@@ -4,13 +4,13 @@ The nanoparticle com for this situation (x, y, z_interface)
 The z valu is only used for droping the oda which are not at interface
 """
 
-import sys
 import numpy as np
 import pandas as pd
 
 from common import logger
-from common import xvg_to_dataframe
 from common import my_tools
+from common import xvg_to_dataframe
+from common import static_info as stinfo
 from common.colors_text import TextColor as bcolors
 
 
@@ -36,6 +36,8 @@ class RdfClculation:
         contact_info: pd.DataFrame = self.get_contact_info(log)
         interface_oda: dict[int, np.ndarray] = \
             self.get_interface_oda(contact_info, amino_arr[:-2])
+        oda_distances: dict[int, np.ndarray] = \
+            self.calc_distance_from_np(interface_oda, contact_info, box_dims)
 
     def get_contact_info(self,
                          log: logger.logging.Logger
@@ -47,6 +49,25 @@ class RdfClculation:
         self.inf_msg += f'\tReading `{fname}`\n'
         return pd.read_csv(fname, sep=' ')
 
+    def calc_distance_from_np(self,
+                              interface_oda: dict[int, np.ndarray],
+                              contact_info: pd.DataFrame,
+                              box_dims: dict[str, float]
+                              ) -> dict[int, np.ndarray]:
+        """calculate the oda-np dictances by applying pbc"""
+        l_xy: tuple[float, float] = (box_dims['x_hi'] - box_dims['x_lo'],
+                                     box_dims['y_hi'] - box_dims['y_lo'])
+        oda_distances: dict[int, np.ndarray] = {}
+        for frame, arr in interface_oda.items():
+            distance_i = np.zeros((arr.shape[0], 1))
+            dx_i = arr[:, 0] - self.np_com[:len(arr), 0]
+            dx_pbc = dx_i - (l_xy[0] * np.round(dx_i/l_xy[0]))
+            dy_i = arr[:, 1] - self.np_com[:len(arr), 1]
+            dy_pbc = dy_i - (l_xy[1] * np.round(dy_i/l_xy[1]))
+            distance_i = np.sqrt(dx_pbc**2 + dy_pbc**2)
+            oda_distances[frame] = distance_i
+        return oda_distances
+
     @staticmethod
     def get_interface_oda(contact_info: pd.DataFrame,
                           amino_arr: np.ndarray
@@ -54,14 +75,14 @@ class RdfClculation:
         """get the oda at interface"""
         interface_z: np.ndarray = \
             contact_info['interface_z'].to_numpy().reshape(-1, 1)
-        interface_std: np.float64 = np.std(interface_z)
+        np_radius: float = stinfo.np_info['radius']
         interface_oda: dict[int, np.ndarray] = {}
         for i_frame, frame in enumerate(amino_arr):
             xyz_i: np.ndarray = frame.reshape(-1, 3)
             ind_at_interface: list[int] = []
             ind_at_interface = np.where(
-                (xyz_i[:, 2] < interface_z + interface_std) &
-                (xyz_i[:, 2] > interface_z - interface_std))
+                (xyz_i[:, 2] <= interface_z[i_frame] + np_radius/2) &
+                (xyz_i[:, 2] >= interface_z[i_frame] - np_radius/2))
             interface_oda[i_frame] = xyz_i[ind_at_interface[0]]
         return interface_oda
 

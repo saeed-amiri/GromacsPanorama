eb40cfa63b45069aa829eaeb908fa3bb81dad110
Saeed Amiri <samiri@uni-bremen.de>
2023-12-05 16:13:59 +0100

    update log info

--- src/common/my_tools.py
+++ src/common/my_tools.py
@@ -20,7 +20,7 @@ def check_file_exist(fname: str,  # Name of the file to check
         sys.exit(f'{bcolors.FAIL}{__name__}: '
                  f'(Error! `{fname}` dose not '
                  f'exist \n{bcolors.ENDC}')
-    log.info(msg := f'Checking: `{fname}`')
+    log.info(msg := f'`{fname}` exist.')
     print(f'{bcolors.OKBLUE}my_tools:\n\t{msg}{bcolors.ENDC}\n')
 
 def check_file_extension(fname: str,  # Name of the file to check

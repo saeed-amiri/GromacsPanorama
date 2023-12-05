7466b01dcd8a2747b89bdb919e0bb9d2f06cf68e
Saeed Amiri <samiri@uni-bremen.de>
2023-12-05 16:13:37 +0100

    check the files

--- src/module5_gmx_vmd_ploters/xvg_plotter.py
+++ src/module5_gmx_vmd_ploters/xvg_plotter.py
@@ -8,6 +8,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from common import logger
+from common import my_tools
 from common import plot_tools
 from common import static_info as stinfo
 from common import xvg_to_dataframe as xvg
@@ -17,7 +18,7 @@ from common.colors_text import TextColor as bcolors
 @dataclass
 class XvgBaseConfig:
     """Patameters for the plotting the xvg files"""
-    f_name: list[str]  # Names of the xvg files
+    f_names: list[str]  # Names of the xvg files
     x_column: str  # Name of the x data
     y_columns: list[str]  # Names of the second columns to plots
     out_suffix: str = 'xvg.png'  # Suffix for the output file
@@ -26,7 +27,7 @@ class XvgBaseConfig:
 @dataclass
 class XvgPlotterConfig(XvgBaseConfig):
     """set the parameters"""
-    f_name: list[str] = field(default_factory=lambda: ['coord.xvg'])
+    f_names: list[str] = field(default_factory=lambda: ['coord.xvg'])
     x_column: str = 'frame'
     y_columns: list[str] = field(default_factory=lambda: ['COR_APT_Z'])
 
@@ -41,7 +42,22 @@ class PlotXvg:
                  configs: "XvgPlotterConfig" = XvgPlotterConfig()
                  ) -> None:
         self.configs = configs
+        self.initiate_data(log)
         self._write_msg(log)
+
+    def initiate_data(self,
+                      log: logger.logging.Logger
+                      ) -> None:
+        """check the data and return them in a pd.Dataframe"""
+        self.file_existence(self.configs.f_names, log)
+
+    @staticmethod
+    def file_existence(f_names: list[str],
+                       log: logger.logging.Logger
+                       ) -> None:
+        """check if the files exist"""
+        for fname in f_names:
+            my_tools.check_file_exist(fname, log)
 
     def _write_msg(self,
                    log: logger.logging.Logger  # To log

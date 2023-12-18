2795401ca7085c4e880cc364d35462a7af24af36
Saeed Amiri <samiri@uni-bremen.de>
2023-12-18 11:43:02 +0100

    write the xvg file

--- src/common/my_tools.py
+++ src/common/my_tools.py
@@ -3,6 +3,10 @@ import os
 import re
 import sys
 import typing
+import warnings
+
+import pandas as pd
+
 from common import logger
 from common.colors_text import TextColor as bcolors
 
@@ -81,3 +85,44 @@ def clean_string(input_string: str) -> str:
     cleaned_string = re.sub(r'[^a-zA-Z0-9]+', '_', cleaned_string)
     
     return cleaned_string
+
+def write_xvg(df_i: pd.DataFrame,
+              log: logger.logging.Logger,
+              fname: str = 'df.xvg'
+              ) -> None:
+        """
+        Write the data into xvg format
+        Raises:
+            ValueError: If the DataFrame has no columns.
+        """
+        if df_i.columns.empty:
+            log.error(msg := "\tThe DataFrame has no columns.\n")
+            raise ValueError(f'{bcolors.FAIL}{msg}{bcolors.ENDC}\n')
+        if df_i.empty:
+            log.warning(
+                msg := f"The df is empty. `{fname}` will not contain data.")
+            warnings.warn(msg, UserWarning)
+
+        columns: list[str] = df_i.columns.to_list()
+
+        header_lines: list[str] = [
+            '# Written by my_tools.write_xvg\n',
+            f'# Current directory: {os.getcwd()}',
+            '@   title "Contact information"',
+            '@   xaxis label "Frame index"',
+            '@   yaxis label "Varies"',
+            '@TYPE xy',
+            '@ view 0.15, 0.15, 0.75, 0.85',
+            '@legend on',
+            '@ legend box on',
+            '@ legend loctype view',
+            '@ legend 0.78, 0.8',
+            '@ legend length 2'
+        ]
+        legend_lines: list[str] = \
+            [f'@ s{i} legend "{col}"' for i, col in enumerate(columns)]
+
+        with open(fname, 'w', encoding='utf8') as f_w:
+            for line in header_lines + legend_lines:
+                f_w.write(line + '\n')
+            df_i.to_csv(f_w, sep=' ', index=True, header=None, na_rep='NaN')

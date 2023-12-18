25cc2956a4965f5d8986179d9130cb2b5ac0b993
Saeed Amiri <samiri@uni-bremen.de>
2023-12-18 14:09:41 +0100

    clean up

--- src/module4_analysis_oda/trajectory_oda_analysis.py
+++ src/module4_analysis_oda/trajectory_oda_analysis.py
@@ -5,6 +5,7 @@ This script uses the out put of the module3_aqua_analysis/contact.info
 """
 
 import sys
+from dataclasses import dataclass
 
 from module4_analysis_oda.rdf import RdfCalculationConfig, RdfClculation
 from module4_analysis_oda.oda_density_around_np import \
@@ -22,40 +23,71 @@ from common import logger
 from common.com_file_parser import GetCom
 
 
+@dataclass
+class ComputationCalculations:
+    """To select computions"""
+    plain_rdf: bool = False
+    density_rdf: bool = True
+    contrast_density: bool = False
+
+
 class OdaAnalysis:
     """call all the other scripts here"""
     def __init__(self,
                  fname: str,  # Name of the com file
-                 log: logger.logging.Logger
+                 log: logger.logging.Logger,
+                 compute_config: "ComputationCalculations" = \
+                 ComputationCalculations()
                  ) -> None:
-        parsed_com = GetCom(fname)
-        self.initiate(parsed_com, log)
+        self.parsed_com = GetCom(fname)
+        self.compute_config = compute_config
+        self.initiate(log)
 
     def initiate(self,
-                 parsed_com: "GetCom",
                  log: logger.logging.Logger
                  ) -> None:
         """call the scripts"""
-        # rdf_config = RdfCalculationConfig(
-            # amino_arr=parsed_com.split_arr_dict['AMINO_ODN'],
-            # box_dims=parsed_com.box_dims)
-        # RdfClculation(log, rdf_config)
-        for residue in ['AMINO_ODN', 'CLA']:
-            params: "ParameterConfig" = \
-                ParameterConfig(number_of_regions=50,
-                                time_dependent_step=101,
-                                xvg_output=f'{residue}_densities.xvg')
-            oda_density = SurfactantDensityAroundNanoparticle(
-                parsed_com.split_arr_dict[residue],
-                log,
-                param_config=params,
-                residue=residue)
-            SurfactantDensityPlotter(oda_density, log, residue=residue)
-        # contrast = SurfactantsLocalizedDensityContrast(
-        #     parsed_com.split_arr_dict['AMINO_ODN'], log)
-        # SurfactantContrastDensityPlotter(contrast.number_density, log)
-        # PlotSurfactantComXY(parsed_com.split_arr_dict['AMINO_ODN'], log)
-        
+        self.plain_rdf_calculation(log)
+        self.compute_densities(log)
+        self.compute_contrast_density(log)
+
+    def plain_rdf_calculation(self,
+                              log: logger.logging.Logger
+                              ) -> None:
+        """calculate rdf (legecy)"""
+        if self.compute_config.plain_rdf:
+            rdf_config = RdfCalculationConfig(
+                amino_arr=self.parsed_com.split_arr_dict['AMINO_ODN'],
+                box_dims=self.parsed_com.box_dims)
+            RdfClculation(log, rdf_config)
+
+    def compute_densities(self,
+                          log: logger.logging.Logger
+                          ) -> None:
+        """compute density and rdf"""
+        if self.compute_config.density_rdf:
+            for residue in ['AMINO_ODN', 'CLA']:
+                params: "ParameterConfig" = \
+                    ParameterConfig(number_of_regions=50,
+                                    time_dependent_step=101,
+                                    xvg_output=f'{residue}_densities.xvg')
+                oda_density = SurfactantDensityAroundNanoparticle(
+                    self.parsed_com.split_arr_dict[residue],
+                    log,
+                    param_config=params,
+                    residue=residue)
+                SurfactantDensityPlotter(oda_density, log, residue=residue)
+
+    def compute_contrast_density(self,
+                                 log: logger.logging.Logger
+                                 ) -> None:
+        """compute the contrast density for the oda"""
+        if self.compute_config.contrast_density:
+            contrast = SurfactantsLocalizedDensityContrast(
+                self.parsed_com.split_arr_dict['AMINO_ODN'], log)
+            SurfactantContrastDensityPlotter(contrast.number_density, log)
+            PlotSurfactantComXY(self.parsed_com.split_arr_dict['AMINO_ODN'],
+                                log)
 
 
 if __name__ == '__main__':

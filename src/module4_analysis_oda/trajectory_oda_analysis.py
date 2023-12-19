93375c4ad43b56f3bfafda85057d09c9aaf19855
Saeed Amiri <samiri@uni-bremen.de>
2023-12-19 11:09:43 +0100

    clean up; PEP8

--- src/module4_analysis_oda/trajectory_oda_analysis.py
+++ src/module4_analysis_oda/trajectory_oda_analysis.py
@@ -10,19 +10,19 @@ from collections import namedtuple
 
 from module4_analysis_oda.rdf import RdfCalculationConfig, RdfClculation
 from module4_analysis_oda.oda_density_around_np import \
-    OdaInputFilesConfig, ParameterConfig, SurfactantDensityAroundNanoparticle
+    ParameterConfig, SurfactantDensityAroundNanoparticle
 from module4_analysis_oda.oda_density_around_np_plotter import \
-    DensityHeatMapConfig, DensityGraphConfig, Rdf2dGraphConfig, \
     SurfactantDensityPlotter
 from module4_analysis_oda.oda_density_contrast import \
-    InputFilesConfig, SurfactantsLocalizedDensityContrast
+    SurfactantsLocalizedDensityContrast
 from module4_analysis_oda.oda_density_contrast_plotter import \
-    GraphConfing, SurfactantContrastDensityPlotter
+    SurfactantContrastDensityPlotter
 from module4_analysis_oda.oda_xy_plotter import PlotSurfactantComXY
 from module4_analysis_oda.density_overlay_plotter import OverlayPlotDensities
 
 from common import logger
 from common.com_file_parser import GetCom
+from common.colors_text import TextColor as bcolors
 
 
 FitTurns: "namedtuple" = \
@@ -40,6 +40,9 @@ class ComputationCalculations:
 
 class OdaAnalysis:
     """call all the other scripts here"""
+
+    fit_turns: "FitTurns"
+
     def __init__(self,
                  fname: str,  # Name of the com file
                  log: logger.logging.Logger,
@@ -54,53 +57,60 @@ class OdaAnalysis:
                  log: logger.logging.Logger
                  ) -> None:
         """call the scripts"""
-        self.plain_rdf_calculation(log)
-        self.compute_plot_densities(log)
-        self.compute_contrast_density(log)
-        self.plot_overlay_densities(log)
+        try:
+            if self.compute_config.plain_rdf:
+                self.plain_rdf_calculation(log)
+
+            if self.compute_config.density_rdf:
+                self.compute_plot_densities(log)
+
+            if self.compute_config.contrast_density:
+                self.compute_contrast_density(log)
+
+            if self.compute_config.overlay_plot:
+                self.plot_overlay_densities(log)
+
+        except Exception as err:
+            log.error(f"An error occurred during ODA analysis: {err}")
 
     def plain_rdf_calculation(self,
                               log: logger.logging.Logger
                               ) -> None:
         """calculate rdf (legecy)"""
-        if self.compute_config.plain_rdf:
-            rdf_config = RdfCalculationConfig(
-                amino_arr=self.parsed_com.split_arr_dict['AMINO_ODN'],
-                box_dims=self.parsed_com.box_dims)
-            RdfClculation(log, rdf_config)
+        rdf_config = RdfCalculationConfig(
+            amino_arr=self.parsed_com.split_arr_dict['AMINO_ODN'],
+            box_dims=self.parsed_com.box_dims)
+        RdfClculation(log, rdf_config)
 
     def compute_plot_densities(self,
                                log: logger.logging.Logger
                                ) -> None:
         """compute density and rdf"""
-        if self.compute_config.density_rdf:
-            for residue in ['AMINO_ODN', 'CLA']:
-                params: "ParameterConfig" = \
-                    ParameterConfig(number_of_regions=50,
-                                    time_dependent_step=101,
-                                    xvg_output=f'{residue}_densities.xvg')
-                oda_density = SurfactantDensityAroundNanoparticle(
-                    self.parsed_com.split_arr_dict[residue],
-                    log,
-                    param_config=params,
-                    residue=residue)
-                SurfactantDensityPlotter(oda_density, log, residue=residue)
-                if residue == 'AMINO_ODN':
-                    self.fit_turns = \
-                        FitTurns(first_turn=oda_density.first_turn,
-                                      midpoint=oda_density.midpoint,
-                                      second_turn=oda_density.second_turn)
+        for residue in ['AMINO_ODN', 'CLA']:
+            params: "ParameterConfig" = \
+                ParameterConfig(number_of_regions=50,
+                                time_dependent_step=101,
+                                xvg_output=f'{residue}_densities.xvg')
+            oda_density = SurfactantDensityAroundNanoparticle(
+                self.parsed_com.split_arr_dict[residue],
+                log,
+                param_config=params,
+                residue=residue)
+            SurfactantDensityPlotter(oda_density, log, residue=residue)
+            if residue == 'AMINO_ODN':
+                self.fit_turns = \
+                    FitTurns(first_turn=oda_density.first_turn,
+                             midpoint=oda_density.midpoint,
+                             second_turn=oda_density.second_turn)
 
     def compute_contrast_density(self,
                                  log: logger.logging.Logger
                                  ) -> None:
         """compute the contrast density for the oda"""
-        if self.compute_config.contrast_density:
-            contrast = SurfactantsLocalizedDensityContrast(
-                self.parsed_com.split_arr_dict['AMINO_ODN'], log)
-            SurfactantContrastDensityPlotter(contrast.number_density, log)
-            PlotSurfactantComXY(self.parsed_com.split_arr_dict['AMINO_ODN'],
-                                log)
+        contrast = SurfactantsLocalizedDensityContrast(
+            self.parsed_com.split_arr_dict['AMINO_ODN'], log)
+        SurfactantContrastDensityPlotter(contrast.number_density, log)
+        PlotSurfactantComXY(self.parsed_com.split_arr_dict['AMINO_ODN'], log)
 
     def plot_overlay_densities(self,
                                log: logger.logging.Logger
@@ -109,14 +119,23 @@ class OdaAnalysis:
         Plot the overlay densities (density, rdf, ... for ODA and
         Cl.
         """
-        if self.compute_config.overlay_plot:
-            OverlayPlotDensities(
-                {'AMINO_ODN_densities.xvg': 'ODA', 'CLA_densities.xvg': 'Cl'},
-                self.fit_turns,
-                log)
-
+        if not self.compute_config.density_rdf:
+            log.error(msg := ('\n\tError! Needs the turns points from '
+                              '`SurfactantDensityAroundNanoparticle`\n'))
+            sys.exit(f'{bcolors.FAIL}{msg}{bcolors.ENDC}')
+        OverlayPlotDensities(
+            {'AMINO_ODN_densities.xvg': 'ODA', 'CLA_densities.xvg': 'Cl'},
+            self.fit_turns,
+            log)
 
 
 if __name__ == '__main__':
-    OdaAnalysis(fname=sys.argv[1],
-                log=logger.setup_logger('oda_analysing.log'))
+    LOG = logger.setup_logger('oda_analysing.log')
+    try:
+        analysis = OdaAnalysis(fname=sys.argv[1], log=LOG)
+    except IndexError:
+        LOG.error("No command line argument provided for the filename.")
+        sys.exit(1)
+    else:
+        LOG.error("Failed to initialize ODA analysis")
+        sys.exit(1)

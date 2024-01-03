32a08214e556261b16d33c71cf112e70e5113cc7
Saeed Amiri <samiri@uni-bremen.de>
2024-01-03 12:13:57 +0100

    initiate copmuting rdf: doc string

---
+++ src/module6_charge_analysis/densities_around_np.py
@@ -0,0 +1,30 @@
+"""
+densities_around_np.py
+
+This module is designed to calculate various properties such as
+densities, radial distribution functions (rdf), and cumulative
+distribution functions (cdf) for ions or other residues in the vicinity
+of a nanoparticle. Unlike the 'oda_density_around_np.py' script in
+module4, which focuses on two-dimensional analyses,
+'densities_around_np.py' extends these computations to three dimensions.
+This enhancement allows for a more comprehensive and spatially detailed
+understanding of the distribution and interactions of ions or residues
+around nanoparticles in a 3D space.
+
+Functions:
+    - calculate_density: Computes the density distribution of ions or
+        residues around the nanoparticle in 3D.
+
+    - compute_rdf: Calculates the radial distribution function, providing
+    insights into the spatial distribution and local ordering of particles.
+
+    - compute_cdf: Determines the cumulative distribution function,
+    offering a cumulative perspective of particle distribution up to a
+    certain radius.
+
+This script facilitates a deeper exploration of the structural and
+dynamic properties of systems involving nanoparticles and their
+surrounding environment in three-dimensional space.
+Jan 3, 2023
+Saeed
+"""

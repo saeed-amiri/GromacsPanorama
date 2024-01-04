0c07fd770cae5ecd7455afce0d844fd85191bffa
Saeed Amiri <samiri@uni-bremen.de>
2024-01-04 10:34:36 +0100

    calculate densities

--- src/module6_charge_analysis/charge_analysis_interface_np.py
+++ src/module6_charge_analysis/charge_analysis_interface_np.py
@@ -35,7 +35,7 @@ from common import logger
 from common.com_file_parser import GetCom
 from common.colors_text import TextColor as bcolors
 
-from module6_charge_analysis import np_charge_analysis
+from module6_charge_analysis import np_charge_analysis, densities_around_np
 
 
 @dataclass
@@ -78,6 +78,8 @@ class ComputeCharges:
         """
         cla_arr: np.ndarray = self.parsed_com.split_arr_dict['CLA']
         np_charge_analysis.NpChargeAnalysis(cla_arr, self.config, log)
+        densities_around_np.ResidueDensityAroundNanoparticle(
+            cla_arr, log, 'CLA')
 
     def write_msg(self,
                   log: logger.logging.Logger  # To log

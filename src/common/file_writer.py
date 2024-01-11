2bd736ad88a39a55197a2d5ab42d0e0e1fb486dc
Saeed Amiri <samiri@uni-bremen.de>
2024-01-11 16:00:47 +0100

    write functions

---
+++ src/common/file_writer.py
@@ -0,0 +1,52 @@
+"""helper functions to write files with different formats:
+xvg, gro, pdb, ...
+"""
+
+import os
+import warnings
+import pandas as pd
+
+from common import logger
+from common.colors_text import TextColor as bcolors
+
+
+def write_xvg(df_i: pd.DataFrame,
+              log: logger.logging.Logger,
+              fname: str = 'df.xvg'
+              ) -> None:
+    """
+    Write the data into xvg format
+    Raises:
+        ValueError: If the DataFrame has no columns.
+    """
+    if df_i.columns.empty:
+        log.error(msg := "\tThe DataFrame has no columns.\n")
+        raise ValueError(f'{bcolors.FAIL}{msg}{bcolors.ENDC}\n')
+    if df_i.empty:
+        log.warning(
+            msg := f"The df is empty. `{fname}` will not contain data.")
+        warnings.warn(msg, UserWarning)
+
+    columns: list[str] = df_i.columns.to_list()
+
+    header_lines: list[str] = [
+        f'# Written by {write_xvg.__module__}',
+        f"# Current directory: {os.getcwd()}",
+        '@   title "Contact information"',
+        '@   xaxis label "Frame index"',
+        '@   yaxis label "Varies"',
+        '@TYPE xy',
+        '@ view 0.15, 0.15, 0.75, 0.85',
+        '@legend on',
+        '@ legend box on',
+        '@ legend loctype view',
+        '@ legend 0.78, 0.8',
+        '@ legend length 2'
+    ]
+    legend_lines: list[str] = \
+        [f'@ s{i} legend "{col}"' for i, col in enumerate(df_i.columns)]
+
+    with open(fname, 'w', encoding='utf8') as f_w:
+        for line in header_lines + legend_lines:
+            f_w.write(line + '\n')
+        df_i.to_csv(f_w, sep=' ', index=True, header=None, na_rep='NaN')

4239b496163ba1f5a966ec7c5f8063ade0019459
Saeed Amiri <samiri@uni-bremen.de>
2024-01-18 09:29:30 +0100

    set up configurations

--- src/module5_gmx_vmd_ploters/order_parameter_plotter.py
+++ src/module5_gmx_vmd_ploters/order_parameter_plotter.py
@@ -24,3 +24,102 @@ Opt. by ChatGpt
 Saeed
 18 Jan 2023
 """
+
+
+import typing
+from dataclasses import dataclass, field
+
+
+@dataclass
+class BaseConfig:
+    """
+    Basic configurations and setup for the plots.
+    """
+    # pylint: disable=too-many-instance-attributes
+    graph_suffix: str = 'order_parameter.png'
+
+    labels: dict[str, str] = field(default_factory=lambda: {
+        'title': 'Computed order parameter',
+        'ylabel': 'S',
+        'xlabel': 'Nr. Oda'
+    })
+
+    graph_styles: dict[str, typing.Any] = field(default_factory=lambda: {
+        'label': 'S',
+        'color': 'black',
+        'marker': 'o',
+        'linestyle': '--',
+        'markersize': 5,
+    })
+
+    line_styles: list[str] = \
+        field(default_factory=lambda: ['-', ':', '--', '-.'])
+    colors: list[str] = \
+        field(default_factory=lambda: ['black', 'red', 'blue', 'green'])
+
+    height_ratio: float = (5 ** 0.5 - 1) * 2
+
+    y_unit: str = 'S'
+
+    log_x_axis: bool = False
+
+
+@dataclass
+class SimpleGraph(BaseConfig):
+    """
+    Parameters for simple data plots.
+    """
+
+
+@dataclass
+class ErrorBarGraph(BaseConfig):
+    """
+    Parameters for simple data with error bar
+    """
+    graph_suffix: str = 'order_parameter_err.png'
+
+
+@dataclass
+class DoubleDataGraph(BaseConfig):
+    """
+    Parameters for the plotting both the data with and without NP
+    """
+    graph_suffix: str = 'order_parameter_both.png'
+
+
+@dataclass
+class BarPlotConfig(BaseConfig):
+    """
+    Parameters for the number of oda at the interface
+    """
+    graph_suffix: str = 'bar_oda_nr.png'
+
+
+@dataclass
+class FileConfig:
+    """
+    Set the name of the input files for plot with labels say what are
+    those
+    """
+    fnames: dict[str, str] = field(default_factory=lambda: {
+        'no_np': 'order_parameter.log'
+        })
+
+
+@dataclass
+class ParameterConfig:
+    """
+    Parameters for the plots
+    """
+    box_dimension: tuple[float, float] = (21.7, 21.7)  # xy in nm@dataclass
+
+
+@dataclass
+class AllConfig(FileConfig, ParameterConfig):
+    """
+    Consolidates all configurations for different graph types.
+    """
+    simple_config: SimpleGraph = field(default_factory=SimpleGraph)
+    errbar_config: ErrorBarGraph = field(default_factory=ErrorBarGraph)
+    double_config: DoubleDataGraph = field(default_factory=DoubleDataGraph)
+    bar_config: BarPlotConfig = field(default_factory=BarPlotConfig)

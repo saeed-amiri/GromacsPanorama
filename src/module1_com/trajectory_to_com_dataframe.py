fd51a0ec90f72a7166ba4d4bc43485ca87e65c83
Saeed Amiri <samiri@uni-bremen.de>
2024-01-24 13:48:27 +0100

    update the comments

--- src/module1_com/trajectory_to_com_dataframe.py
+++ src/module1_com/trajectory_to_com_dataframe.py
@@ -151,7 +151,6 @@ class CalculateCom:
 
         self.pickle_arr(com_arr, log)
         current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        print(current_time)
         self.info_msg += f'\n\tEnd at: {current_time}\n'
 
     def pickle_arr(self,

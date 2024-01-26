fdbb7042bad91274f10e283206447bf60fdf1aaf
Saeed Amiri <samiri@uni-bremen.de>
2024-01-26 08:30:31 +0100

    initiate the configurations

--- src/module8_analysis_order_parameter/order_parameter_distribution.py
+++ src/module8_analysis_order_parameter/order_parameter_distribution.py
@@ -17,11 +17,67 @@ Identifying Residues in Bins:
     a specified bin. This step involves determining which residues'
     indices are within the boundaries of each bin.
 
-Calculating Average Order Parameters: Once the relevant residues for
-each bin and frame are identified, we will calculate the average order
-parameter. This involves averaging the order parameters of all selected
-residues within each specific frame and bin.
+Calculating Average Order Parameters:
+    Once the relevant residues for each bin and frame are identified,
+    we will calculate the average order parameter. This involves
+    averaging the order parameters of all selected residues within each
+    specific frame and bin.
+
+Visulising the results:
+    Also plot the different type of graphs for this distributions
+
 Opt. by ChatGpt
 Saeed
 26 Jan 2024
 """
+
+from dataclasses import dataclass
+
+import numpy as np
+
+from common import logger
+from common.colors_text import TextColor as bcolors
+
+
+@dataclass
+class FileConfig:
+    """set the name of the input file"""
+
+
+@dataclass
+class ParameterConfig:
+    """set the prameters for the computations"""
+    axis: str = 'z'
+    nr_bins: int = 50
+
+
+@dataclass
+class AllConfigs(FileConfig, ParameterConfig):
+    """set all the inputs and parameters"""
+
+
+class ComputeOPDistribution:
+    """compute the order parameters distribution through on axis"""
+
+    info_msg: str = 'Message from ComputeOPDistribution:\n'
+
+    def __init__(self,
+                 com_arr: np.ndarray,
+                 orderp_arr: np.ndarray,
+                 log: logger.logging.Logger,
+                 configs: AllConfigs = AllConfigs()
+                 ) -> None:
+        self._write_msg(log)
+
+    def _write_msg(self,
+                   log: logger.logging.Logger  # To log
+                   ) -> None:
+        """write and log messages"""
+        print(f'{bcolors.OKCYAN}{ComputeOPDistribution.__name__}:\n'
+              f'\t{self.info_msg}{bcolors.ENDC}')
+        log.info(self.info_msg)
+
+
+if __name__ == '__main__':
+    print(f'{bcolors.OKGREEN}This script is calling within '
+          f'`order_parameter_analysis.py`\n{bcolors.ENDC}')

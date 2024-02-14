de21c779b96509d131840747fa62a214992262b8
Saeed Amiri <samiri@uni-bremen.de>
2024-02-14 13:50:59 +0100

    update the input for the tcl

--- data/tcl/loading_trr_saving_in.tcl
+++ data/tcl/loading_trr_saving_in.tcl
@@ -1,25 +1,27 @@
-# Load the gro and trr and with optinal number of frames
-set mol [mol new @GRO type gro waitfor all]
-puts "Attempting to load file: $mol"
-set distance 5
+    # Load the gro and trr and with optinal number of frames
+    set mol [mol new @GRO  type @TYPE waitfor all]
+    puts "Attempting to load file: $mol"
+    set distance 5
 
-mol addfile @TRR type trr first @FIRST last @LAST step STEP waitfor all molid $mol
+    mol addfile @TRR type trr first @FIRST last @LAST step @STEP\
+    waitfor all molid $mol
 
-set nf [molinfo top get numframes]
-puts " The numbers of frames are $nf"
-for {set i 0} {$i < $nf} {incr i} {
-    animate goto $i
-    set inRadiusResidues [atomselect top @ATOMSELCT]
-    set resIDs [$inRadiusResidues get residue]
-    if {[llength $resIDs] > 0} {
-        set uniqueResIDs [lsort -unique $resIDs]
-        set completeInRadiusResidues [atomselect top "@FINALSELECT $uniqueResIDs"]
-        $completeInRadiusResidues @WRITE "@OUTNAME_${i}.@EXTENSION"
-        $completeInRadiusResidues delete
-    } else {
-        puts "No residues within $distance Å of APT or COR in frame $i"
+    set nf [molinfo top get numframes]
+    puts " The numbers of frames are $nf"
+    for {set i 0} {$i < $nf} {incr i} {
+        animate goto $i
+        set inRadiusResidues [atomselect top "@ATOMSELCT"]
+        set resIDs [$inRadiusResidues get residue]
+        if {[llength $resIDs] > 0} {
+            set uniqueResIDs [lsort -unique $resIDs]
+            set completeInRadiusResidues\
+            [atomselect top "@FINALSELECT $uniqueResIDs"]
+            $completeInRadiusResidues @WRITE "@OUTNAME_${i}.@TYPE"
+            $completeInRadiusResidues delete
+        } else {
+            puts "No residues within $distance Å of APT or COR in frame $i"
+        }
+        $inRadiusResidues delete
     }
-    $inRadiusResidues delete
-}
 
-exit
+    exit

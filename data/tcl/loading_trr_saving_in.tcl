8f2716b7a1a2a5c5be405a359f377839eee21f6e
Saeed Amiri <samiri@uni-bremen.de>
2024-02-14 11:55:12 +0100

    input file for vmd

---
+++ data/tcl/loading_trr_saving_in.tcl
@@ -0,0 +1,25 @@
+# Load the gro and trr and with optinal number of frames
+set mol [mol new @GRO type gro waitfor all]
+puts "Attempting to load file: $mol"
+set distance 5
+
+mol addfile @TRR type trr first @FIRST last @LAST step STEP waitfor all molid $mol
+
+set nf [molinfo top get numframes]
+puts " The numbers of frames are $nf"
+for {set i 0} {$i < $nf} {incr i} {
+    animate goto $i
+    set inRadiusResidues [atomselect top @ATOMSELCT]
+    set resIDs [$inRadiusResidues get residue]
+    if {[llength $resIDs] > 0} {
+        set uniqueResIDs [lsort -unique $resIDs]
+        set completeInRadiusResidues [atomselect top "@FINALSELECT $uniqueResIDs"]
+        $completeInRadiusResidues @WRITE "@OUTNAME_${i}.@EXTENSION"
+        $completeInRadiusResidues delete
+    } else {
+        puts "No residues within $distance Å of APT or COR in frame $i"
+    }
+    $inRadiusResidues delete
+}
+
+exit

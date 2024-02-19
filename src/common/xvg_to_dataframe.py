2bbea3d2d12c40fcdc5f243ee163e4561d857085
Saeed Amiri <samiri@uni-bremen.de>
2024-02-19 10:54:23 +0100

    update

--- src/common/xvg_to_dataframe.py
+++ src/common/xvg_to_dataframe.py
@@ -78,6 +78,7 @@ class XvgParser:
         columns_names: list[str] = []
         columns_names.append(self.xaxis)
         columns_names.extend(self.columns_names)
+        print(columns_names)
         if (l_1 := len(columns_names)) != (l_2 := len(data_list[0])):
             msg = (f'\n\tThe number of columns` names: `{l_1}` not the same'
                    f' as the number of data columns: `{l_2}`\n')

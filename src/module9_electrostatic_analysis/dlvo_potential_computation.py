4eab951b1f3d47a3ab197f669d2a64b38bb8a7de
Saeed Amiri <samiri@uni-bremen.de>
2024-02-23 10:25:02 +0100

    reading charges and contact angles

--- src/module9_electrostatic_analysis/dlvo_potential_computation.py
+++ src/module9_electrostatic_analysis/dlvo_potential_computation.py
@@ -66,7 +66,7 @@ import numpy as np
 import pandas as pd
 
 
-from common import logger, itp_to_df, my_tools
+from common import logger, my_tools, xvg_to_dataframe
 from common.colors_text import TextColor as bcolors
 
 
@@ -80,10 +80,107 @@ class FileConfig:
 
 @dataclass
 class ParameterConfig:
-    """set parameters for the phi calculation"""
+    """set parameters for the phi calculation
+    radius of the nanopartcile is mandatory
+    contact angle, is optioanl, it is used in case the contact file is
+    not availabel
+    """
     np_radius: float = 30.0  # In Ångströms
+    avg_contact_angle: float = 36.0  # In Degrees
 
 
 @dataclass
 class AllConfig(FileConfig, ParameterConfig):
     """set all the parameters and confiurations"""
+
+
+class ElectroStaticComputation:
+    """compute the electrostatic potenstial"""
+
+    info_msg: str = 'Message from ElectroStaticComputation:\n'
+    configs: AllConfig
+
+    def __init__(self,
+                 log: logger.logging.Logger,
+                 configs: AllConfig = AllConfig()
+                 ) -> None:
+        self.configs = configs
+        self.compute_density(log)
+        self.write_msg(log)
+
+    def compute_density(self,
+                        log: logger.logging.Logger
+                        ) -> None:
+        """compute the true density for the part inside water"""
+        ChargeDensity(log, self.configs)
+
+    def write_msg(self,
+                  log: logger.logging.Logger  # To log
+                  ) -> None:
+        """write and log messages"""
+        now = datetime.now()
+        self.info_msg += \
+            f'\tTime: {now.strftime("%Y-%m-%d %H:%M:%S")}\n'
+        print(f'{bcolors.OKCYAN}{ElectroStaticComputation.__name__}:\n'
+              f'\t{self.info_msg}{bcolors.ENDC}')
+        log.info(self.info_msg)
+
+
+class ChargeDensity:
+    """compute the true density for the part inside water"""
+    # pylint: disable=too-few-public-methods
+
+    info_msg: str = 'Message from ChargeDensity:\n'
+
+    def __init__(self,
+                 log: logger.logging.Logger,
+                 configs: AllConfig
+                 ) -> None:
+        self._get_density(configs, log)
+        self._write_msg(log)
+
+    def _get_density(self,
+                     configs: AllConfig,
+                     log: logger.logging.Logger
+                     ) -> None:
+        """read the input files and compute the charge desnity"""
+        charge: np.ndarray = \
+            self._get_column(configs.charge_fname, log, column='total')
+        try:
+            contact_angle: np.ndarray = \
+                self._get_column(configs.contact_fname,
+                                 log,
+                                 column='contact_angles',
+                                 if_exit=False)
+        except FileNotFoundError as _:
+            self.info_msg += \
+                (f'\t`{configs.contact_fname}` not found!\n\tAaverage '
+                 f'angle `{configs.avg_contact_angle}` is used.\n')
+            contact_angle = np.zeros(charge.shape)
+            contact_angle += configs.avg_contact_angle
+
+    def _get_column(self,
+                    fname: str,
+                    log: logger.logging.Logger,
+                    column: str,
+                    if_exit: bool = True
+                    ) -> np.ndarray:
+        """read the file and return the column in an array"""
+        df_i: pd.DataFrame = \
+            xvg_to_dataframe.XvgParser(fname, log, if_exit=if_exit).xvg_df
+        return df_i[column].to_numpy(dtype=float)
+
+    def _write_msg(self,
+                   log: logger.logging.Logger  # To log
+                   ) -> None:
+        """write and log messages"""
+        now = datetime.now()
+        self.info_msg += \
+            f'\tTime: {now.strftime("%Y-%m-%d %H:%M:%S")}\n'
+        print(f'{bcolors.OKCYAN}{ChargeDensity.__name__}:\n'
+              f'\t{self.info_msg}{bcolors.ENDC}')
+        log.info(self.info_msg)
+
+
+if __name__ == '__main__':
+    ElectroStaticComputation(logger.setup_logger('electro_pot.log'))

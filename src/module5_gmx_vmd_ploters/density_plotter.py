b79a087818d676d2d1e087b144f7f7adf16bef52
Saeed Amiri <samiri@uni-bremen.de>
2024-03-26 10:35:11 +0100

    initiate plotting densities

---
+++ src/module5_gmx_vmd_ploters/density_plotter.py
@@ -0,0 +1,123 @@
+"""
+Plotting density of the residues in the system
+it reads the xvg output files from GRMACS and plot them
+
+"""
+
+import typing
+from dataclasses import dataclass, field
+
+import numpy as np
+import pandas as pd
+
+import matplotlib
+import matplotlib.pylab as plt
+import matplotlib.ticker as tck
+from matplotlib.ticker import FuncFormatter
+
+from common import logger, plot_tools, xvg_to_dataframe
+from common.colors_text import TextColor as bcolors
+
+
+@dataclass
+class BaseGraphConfig:
+    """base class for the graph configuration"""
+    # pylint: disable=too-many-instance-attributes
+    graph_suffix: str = 'denisty.png'
+    y_col_name: str = 'density'
+    xcol_name: str = 'coordinate (nm)'
+
+    labels: dict[str, str] = field(default_factory=lambda: {
+        'ylabel': 'denisty',
+        'xlabel': 'coordinates (z) [nm]'
+    })
+
+    titles: dict[str, typing.Any] = field(default_factory=lambda: {
+    })
+
+    graph_styles: dict[str, typing.Any] = field(default_factory=lambda: {
+        'marker': 'o',
+        'markersize': 0,
+    })
+
+    legends: dict[str, str] = \
+        field(default_factory=lambda: {
+            'CLA': 'Cl',
+            'amino_n': 'N (APTES)',
+            'N': 'N (APTES)',
+            'amino_charge': r'H$^+$ (APTES)',
+            'SOL': 'O (Water)',
+            'D10': 'Decane',
+            'C5': r'C$_5$ (Decane)',
+            'APT': 'N (APTES)',
+            'NH2': 'N (ODA)',
+            'POT': 'Na',
+            'OH2': 'O (water)',
+            'ODN': 'ODA'})
+
+    line_styles: dict[str, str] = \
+        field(default_factory=lambda: {
+            'CLA': '-',
+            'amino_n': '--',
+            'N': '--',
+            'amino_charge': '--',
+            'SOL': '-',
+            'D10': '-',
+            'C5': '-',
+            'APT': '-',
+            'POT': '-',
+            'OH2': '-',
+            'NH2': '-.',
+            'ODN': '-.'})
+
+    colors: dict[str, str] = \
+        field(default_factory=lambda: {
+            'CLA': 'green',
+            'amino_n': 'blue',
+            'N': 'blue',
+            'amino_charge': 'blue',
+            'SOL': 'red',
+            'D10': 'cyan',
+            'C5': 'cyan',
+            'APT': 'k',
+            'POT': 'brown',
+            'OH2': 'red',
+            'ODN': 'orange',
+            'NH2': 'orange'})
+
+    height_ratio: float = (5 ** 0.5 - 1) * 1.5
+
+    y_unit: str = ''
+
+    legend_loc: str = 'lower right'
+
+
+@dataclass
+class FileConfig:
+    """setups for the input files names and their columns name"""
+    # pylint: disable=too-many-instance-attributes
+
+    normalize_type: str = 'max'  # or any other words neside max!
+
+    files: dict[str, dict[str, typing.Any]] = field(
+        default_factory=lambda: {
+            'dens_0': {'fname': 'APT.xvg', 'y_col': 'APT'},
+            'dens_1': {'fname': 'CLA.xvg', 'y_col': 'CLA'},
+            'dens_2': {'fname': 'COR_APT.xvg', 'y_col': 'COR_APT'},
+            'dens_3': {'fname': 'COR.xvg', 'y_col': 'COR'},
+            'dens_4': {'fname': 'D10.xvg', 'y_col': 'D10'},
+            'dens_5': {'fname': 'ODN.xvg', 'y_col': 'ODN'},
+            'dens_6': {'fname': 'POT.xvg', 'y_col': 'POT'},
+            'dens_7': {'fname': 'SOL.xvg', 'y_col': 'SOL'},
+            'dens_8': {'fname': '.xvg', 'y_col': 'amino_charge'}
+            })
+    plot_list: list[int] = \
+        field(default_factory=lambda: [1, 2, 3, 4, 5, 6])
+    legend_loc: str = 'lower right'
+    window_legend_loc: str = 'upper left'
+    max_indicator: str = 'dens_5'
+
+
+@dataclass
+class AllGraphConfig(BaseGraphConfig, FileConfig):
+    """all the configurations for the graph"""

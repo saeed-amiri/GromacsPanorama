eb046c70bc8aad70edcfa2ab85b74fd58b5b07ee
Saeed Amiri <samiri@uni-bremen.de>
2024-04-02 11:37:35 +0200

    set optoins for plot and change unit to nm

--- src/module3_analysis_aqua/surface_plotter.py
+++ src/module3_analysis_aqua/surface_plotter.py
@@ -25,6 +25,9 @@ class PlotConfig:
     fout_suffix: str = 'surface.png'
     nr_fout: int = 5  # Numbers pics in case the list is empty
     indices: typing.Optional[list[int]] = None
+    show_ticks: bool = False
+    show_labels: bool = False
+    show_legend: bool = False
 
 
 @dataclass
@@ -53,7 +56,7 @@ class SurfPlotter:
         selected_frames: dict[int, np.ndarray] = self.get_selected_frames(
             data_config.surf_dict, plot_config.indices, plot_config.nr_fout)
         self.plot_surface(
-            selected_frames, data_config.box_dims, plot_config.fout_suffix)
+            selected_frames, data_config.box_dims, plot_config)
         self._write_msg(log)
         self.selected_frames: list[int] = list(selected_frames.keys())
 
@@ -71,32 +74,59 @@ class SurfPlotter:
     def plot_surface(self,
                      selected_frames: dict[int, np.ndarray],
                      box_dims: dict[str, float],
-                     fout_suffix: str
+                     plot_config: "PlotConfig"
                      ) -> None:
         """plot the surface"""
-        self.info_msg += f'\tThe suffix of the files is: `{fout_suffix}`\n'
+        self.info_msg += \
+            f'\tThe suffix of the files is: `{plot_config.fout_suffix}`\n'
         for frame, value in selected_frames.items():
             fig_i, ax_i = \
-                plot_tools.mk_canvas((0, 200), height_ratio=5**0.5-1)
-            scatter = ax_i.scatter(value[:, 0], value[:, 1], c=value[:, 2],
-                                   s=15, label=f'frame: {frame}')
+                plot_tools.mk_canvas((0, 20), height_ratio=5**0.5-1)
+            if plot_config.show_legend:
+                scatter = ax_i.scatter(value[:, 0] / 10.0,  # Convert to nm
+                                       value[:, 1] / 10.0,  # Convert to nm
+                                       c=value[:, 2] / 10.0,  # Convert to nm
+                                       s=15,
+                                       label=f'frame: {frame}')
+            else:
+                scatter = ax_i.scatter(value[:, 0] / 10.0,
+                                       value[:, 1] / 10.0,
+                                       c=value[:, 2] / 10.0,
+                                       s=15)
+
             # 'left', 'bottom', 'width', 'height'
-            cbar_ax = fig_i.add_axes([.80, 0.15, 0.25, 0.7])
+            cbar_ax = fig_i.add_axes([.80, 0.15, 0.1, 0.5])
             cbar = fig_i.colorbar(scatter, cax=cbar_ax)
             desired_num_ticks = 5
             cbar.ax.yaxis.set_major_locator(MaxNLocator(desired_num_ticks))
 
-            cbar.set_label('Z-Coordinate [A]', rotation=90)
-            ax_i.set_xlabel('X_Coordinate [A]')
-            ax_i.set_ylabel('Y_Coordinate [A]')
+            cbar.set_label('Z-Coordinate [nm]', rotation=90)
+            if plot_config.show_labels:
+                ax_i.set_xlabel('X_Coordinate [nm]')
+                ax_i.set_ylabel('Y_Coordinate [nm]')
+            else:
+                ax_i.set_xlabel('')
+                ax_i.set_ylabel('')
+            if not plot_config.show_ticks:
+                ax_i.set_xticks([])
+                ax_i.set_yticks([])
+            ax_i.spines['bottom'].set_visible(False)
+            ax_i.spines['left'].set_visible(False)
+            ax_i.spines['top'].set_visible(False)
+            ax_i.spines['right'].set_visible(False)
             self.add_circle_to_axis(ax_i,
-                                    origin=self.np_com[frame][:2],
-                                    radius=stinfo.np_info['radius'])
-            ax_i.set_xlim(box_dims['x_lo'] - 7, box_dims['x_hi'] + 7)
-            ax_i.set_ylim(box_dims['y_lo'] - 7, box_dims['y_hi'] + 7)
+                                    origin=self.np_com[frame][:2] / 10.0,
+                                    radius=stinfo.np_info['radius'] / 10.0)
+            ax_i.set_xlim(box_dims['x_lo'] / 10.0 - 0.7,
+                          box_dims['x_hi'] / 10.0 + 0.7)
+            ax_i.set_ylim(box_dims['y_lo'] / 10.0 - 0.7,
+                          box_dims['y_hi'] / 10.0 + 0.7)
             plt.gca().set_aspect('equal')
             plot_tools.save_close_fig(
-                fig_i, ax_i, fname=f'{frame}_{fout_suffix}', legend=False)
+                fig_i,
+                ax_i,
+                fname=f'{frame}_{plot_config.fout_suffix}',
+                legend=False)
 
     def add_circle_to_axis(self,
                            ax_i: "matplotlib.axes._subplots.AxesSubplot",

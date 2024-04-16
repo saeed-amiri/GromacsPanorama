821d13ce21fd02d02f3f27cf9d724e7bcea0f783
Saeed Amiri <samiri@uni-bremen.de>
2024-04-16 13:00:17 +0200

    move configs from the main file

--- src/module8_analysis_order_parameter/order_parameter_trr_analysis.py
+++ src/module8_analysis_order_parameter/order_parameter_trr_analysis.py
@@ -77,97 +77,16 @@ and save the data to the xvg file.
 15 April 2024
 """
 
-import os
 import sys
-import typing
-from enum import Enum
-from dataclasses import dataclass, field
-
-import multiprocessing as mp
 
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
 
 import MDAnalysis as mda
 
 from common import logger, xvg_to_dataframe, my_tools, cpuconfig
 from common.colors_text import TextColor as bcolors
-
-
-class ResidueName(Enum):
-    """Residue names for the residues in the trajectory"""
-    # pylint: disable=invalid-name
-    OIL = 'D10'
-    WATER = 'SOL'
-    SURFACTANT = 'ODN'
-
-
-@dataclass
-class OrderParameterConfig:
-    """Order parameter dataclass"""
-    resideu_name: typing.Union[None, ResidueName] = field(init=False)
-    atom_selection: typing.Union[None, str, list[str]] = field(init=False)
-    order_parameter_avg: float = field(init=False)
-    order_parameter_std: float = field(init=False)
-    order_parameter_data: np.ndarray = field(init=False)
-
-
-@dataclass
-class InterfaceConfig:
-    """Interface configuration dataclass"""
-    interface_location: float = 0.0
-    interface_location_std: float = 0.0
-    interface_location_data: np.ndarray = np.array([])
-
-
-@dataclass
-class InputFiles:
-    """Input files dataclass"""
-    box_file: str = 'box.xvg'
-    tpr_file: str = field(init=False)
-    trajectory_file: str = field(init=False)
-    interface_location_file: str = 'contact.xvg'
-    path_name: str = '/scratch/saeed/GÖHBP/PRE_DFG_7May24/single_np/15Oda/data'
-
-    def __post_init__(self) -> None:
-        """Post init function"""
-        self.interface_location_file = \
-            os.path.join(self.path_name, self.interface_location_file)
-        self.box_file = os.path.join(self.path_name, self.box_file)
-
-
-@dataclass
-class ResiduesTails:
-    """Residues tails atoms to compute the order parameter
-    For water use average location of the hydrogen atoms as one tail
-    """
-    # pylint: disable=invalid-name
-    OIL: dict[str, typing.Union[str, list[str]]] = \
-        field(default_factory=lambda: ({'head': 'C1',
-                                        'tail': 'C9'}))
-    WATER: dict[str, typing.Union[str, list[str]]] = \
-        field(default_factory=lambda: ({'head': 'OH2',
-                                        'tail': ['H1', 'H2']}))
-    SURFACTANT: dict[str, typing.Union[str, list[str]]] = \
-        field(default_factory=lambda: ({'head': 'CT3',
-                                        'tail': 'NH2'}))
-
-
-@dataclass
-class AllConfig(ResiduesTails):
-    """Order parameter options dataclass"""
-    residues_tails: ResiduesTails = field(default_factory=ResiduesTails)
-    interface: InterfaceConfig = field(default_factory=InterfaceConfig)
-    order_parameter: OrderParameterConfig = \
-        field(default_factory=OrderParameterConfig)
-    input_files: InputFiles = field(default_factory=InputFiles)
-
-    # The residue names to compute the order parameter
-    # use the ResidueName enum names
-    selected_res: ResidueName = ResidueName.SURFACTANT
-    # number of the cores for multiprocessing computation
-    n_cores: int = field(init=False)
+from module8_analysis_order_parameter.config_classes_trr import AllConfig
 
 
 class OrderParameter:

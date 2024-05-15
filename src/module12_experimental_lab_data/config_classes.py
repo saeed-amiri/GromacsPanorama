f5c3d31858587613c59217b8e6b64bc6d3c7acf7
Saeed Amiri <samiri@uni-bremen.de>
2024-05-15 10:20:03 +0200

    update for the toroidal radius plot

--- src/module12_experimental_lab_data/config_classes.py
+++ src/module12_experimental_lab_data/config_classes.py
@@ -54,7 +54,37 @@ class CaCovConfig:
 
 
 @dataclass
+class ToroidalConfig:
+    """toroidal radius dataclass"""
+    # pylint: disable=too-many-instance-attributes
+    fout: str = f'toroidal_radius.{elsevier_plot_tools.IMG_FORMAT}'
+    show_mirror_axis: bool = False
+    show_y_label: bool = False
+
+    ytick_labels: list[float] = \
+        field(default_factory=lambda: [30, 40, 50, 60, 70])
+    marker_size: int = field(default_factory=lambda:
+                             elsevier_plot_tools.MARKER_SIZE)
+    colors: list[str] = field(default_factory=lambda:
+                              elsevier_plot_tools.MARKER_COLORS)
+    marker_shape: list[str] = field(default_factory=lambda:
+                                    elsevier_plot_tools.MARKER_SHAPES)
+    line_style: list[str] = field(default_factory=lambda:
+                                  elsevier_plot_tools.LINE_STYLES)
+    line_width: int = elsevier_plot_tools.LINE_WIDTH
+
+    y_label: str = 'Toroidal radius'
+    x_label_surfactant: str = r'c$_{ODA}$ [mM/L]'
+    x_label_salt: str = r'c$_{NaCl}$ [mM/L]'
+
+    y_lims: tuple[int, int] = field(default_factory=lambda: (30, 71))
+
+    show_guid_lines: bool = True
+
+
+@dataclass
 class AllConfig(InputFiles):
     """Analysis configuration dataclass"""
     inf_replacement: float = -3.0
     ca_cov: CaCovConfig = field(default_factory=CaCovConfig)
+    toroidal: ToroidalConfig = field(default_factory=ToroidalConfig)

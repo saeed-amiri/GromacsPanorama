9d30b4df63da18a595bcb3eee86feed066ba08ca
Saeed Amiri <samiri@uni-bremen.de>
2024-05-15 12:37:48 +0200

    plot panels

--- src/module12_experimental_lab_data/plot_toroidal_radius.py
+++ src/module12_experimental_lab_data/plot_toroidal_radius.py
@@ -52,30 +52,71 @@ class ToroidalRadiusPlot:
         fig_i, axs = elsevier_plot_tools.mk_canvas_multi('double_column',
                                                          n_rows=1,
                                                          n_cols=3)
-        self._plot_toroidal_radius(grouped_over_surfactant,
-                                   axs[1],
-                                   'surfactant_concentration_mM_L')
-        self._plot_toroidal_radius(grouped_over_salt,
-                                   axs[0],
-                                   'salt_concentration_mM_L')
+        self._plot_panel_a(axs[0], grouped_over_salt)
+        self._plot_panel_b(axs[1], grouped_over_surfactant)
+
         self._save_fig(fig_i)
 
+    def _plot_panel_a(self,
+                      ax_i: plt.Axes,
+                      grouped: "pd.core.groupby.generic.DataFrameGroupBy"
+                      ) -> None:
+        """Plot the toroidal radius for the different salt concentration"""
+        self._plot_toroidal_radius(ax_i,
+                                   grouped,
+                                   'log_surfactant_concentration_mM_L')
+        self._set_x_ticks(ax_i,
+                          grouped,
+                          'surfactant_concentration_mM_L')
+
+    def _plot_panel_b(self,
+                      ax_i: plt.Axes,
+                      grouped: "pd.core.groupby.generic.DataFrameGroupBy"
+                      ) -> None:
+        """Plot the toroidal radius for the different surfactant
+        concentration"""
+        self._plot_toroidal_radius(ax_i,
+                                   grouped,
+                                   'salt_concentration_mM_L')
+        self._set_x_ticks(ax_i,
+                          grouped,
+                          'salt_concentration_mM_L')
+
     def _plot_toroidal_radius(self,
+                              ax_i: plt.Axes,
                               grouped:
                               "pd.core.groupby.generic.DataFrameGroupBy",
-                              ax_i: plt.Axes,
                               x_column: str
                               ) -> None:
         """Plot the toroidal radius"""
-        for name, group in grouped:
-            ax_i.plot(group[x_column],
-                      group['toroidal_radius_nm'],
+        for i, (name, group) in enumerate(grouped):
+            i_index: int = 0
+            if x_column != 'salt_concentration_mM_L':
+                i_index = 1
+            ax_i.plot(group[x_column][i_index:],
+                      group['toroidal_radius_nm'][i_index:],
                       label=f'{name} mM/L',
                       linestyle=self.config.line_style[3],
-                      marker=self.config.marker_shape[2],
+                      marker=self.config.marker_shape[i],
                       ms=self.config.marker_size,
                       lw=self.config.line_width / 2,
-                      color=self.config.colors[0])
+                      color=self.config.colors[i])
+
+    def _set_x_ticks(self,
+                     ax_i: plt.Axes,
+                     grouped: "pd.core.groupby.generic.DataFrameGroupBy",
+                     x_column: str
+                     ) -> None:
+        """Set the x ticks"""
+        _, first_group = next(iter(grouped))
+        if x_column == 'salt_concentration_mM_L':
+            xticks = first_group[x_column]
+            xticks_labels = first_group[x_column]
+        else:
+            xticks = first_group[f'log_{x_column}'][1:]
+            xticks_labels = first_group[x_column][1:]
+        ax_i.set_xticks(xticks)
+        ax_i.set_xticklabels(xticks_labels)
 
     def _save_fig(self,
                   fig_i: plt.Figure

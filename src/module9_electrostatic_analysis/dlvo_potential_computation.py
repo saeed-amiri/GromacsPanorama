d398ee04486c47b00ae8cc9740615527f413a350
Saeed Amiri <samiri@uni-bremen.de>
2024-06-24 12:18:27 +0200

    fix typo in the equation

--- src/module9_electrostatic_analysis/dlvo_potential_computation.py
+++ src/module9_electrostatic_analysis/dlvo_potential_computation.py
@@ -294,12 +294,14 @@ class ElectroStaticComputation:
         """solve the equation 4.25 from pp. 101, Surface and Interfacial
         Forces, H-J Burr and M.Kappl
         """
+        y_initial_guess: float = 1.0
         return fsolve(
-            self._nonlinear_grahame_equation,
-            y_0, args=(a_kappa, co_factor, sigma))
+            self._nonlinear_grahame_ohshima_equation,
+            y_initial_guess, args=(y_0, a_kappa, co_factor, sigma))
 
     @staticmethod
-    def _nonlinear_grahame_equation(y_0: float,
+    def _nonlinear_grahame_equation(phi_0: float,
+                                    y_x: float,
                                     a_kappa: float,
                                     co_factor: float,
                                     sigma: float

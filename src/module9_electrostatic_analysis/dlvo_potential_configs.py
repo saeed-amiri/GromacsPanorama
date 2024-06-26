29977c7872d16aff0d420b09611940dae0103aa2
Saeed Amiri <samiri@uni-bremen.de>
2024-06-26 08:11:40 +0200

    update for setting the correct debye length

--- src/module9_electrostatic_analysis/dlvo_potential_configs.py
+++ src/module9_electrostatic_analysis/dlvo_potential_configs.py
@@ -103,9 +103,9 @@ class PlotConfig(FileConfig):
 
     y_unit: str = ''
     y_lims: tuple[float, float] = (0, 260)
-    x_lims: tuple[float, float] = (2.8, 5.1)
+    x_lims: tuple[float, float] = (2.8, 7.7)
 
-    x_ticks: list[float] = field(default_factory=lambda: [3, 4, 5])
+    x_ticks: list[float] = field(default_factory=lambda: [3, 5])
     y_ticks: list[float] = field(default_factory=lambda: [])
 
     legend_loc: str = 'upper right'

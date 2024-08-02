16042ef0a726b260b181c191690652b7f2cca993
santiago <samiri@uni-bremen.de>
2024-08-02 15:59:03 +0200

    fix typo

--- src/module9_electrostatic_analysis/apbs_analysis_average_pot_boltzman_dist.py
+++ src/module9_electrostatic_analysis/apbs_analysis_average_pot_boltzman_dist.py
@@ -22,8 +22,8 @@ class BoltzmanConfig:
         'epsilon': 78.5,  # medium  permittivity,
         'epsilon_0': 8.854187817e-12,   # vacuum permittivity, farads per meter
         'n_avogadro': 6.022e23,  # Avogadro's number
-        'k_boltzman_JK': 1.380649e-23,  # Joules per Kelvin (J/K)
-        'k_boltzman_eVK': 8.617333262145e-5,  # Electronvolts per Kelvin (eV/K)
+        'k_boltzmann_JK': 1.380649e-23,  # Joules per Kelvin (J/K)
+        'k_boltzmann_eVK': 8.617333262145e-5,  # Electronvolts per Kelvin (eV/K)
         'box_xlim': 21.8,  # Length of the box in x direction [nm]
         'box_ylim': 21.8,  # Length of the box in y direction [nm]
         'box_zlim': 22.5,  # Length of the box in z direction [nm] whole box
@@ -40,7 +40,7 @@ class ComputeBoltzmanDistribution:
     # pylint: disable=too-many-arguments
     info_msg: str = 'Message from ComputeBoltzmanDistribution:\n'
     config: BoltzmanConfig
-    boltzman_distribution: dict[int, tuple[np.ndarray, np.ndarray]]
+    boltzmann_distribution: dict[int, tuple[np.ndarray, np.ndarray]]
 
     def __init__(self,
                  cut_radial_average: list[np.ndarray],
@@ -54,21 +54,21 @@ class ComputeBoltzmanDistribution:
             self.make_dict_index_phi(cut_radial_average,
                                      radii_list,
                                      sphere_grid_range)
-        self.boltzman_distribution = \
-            self.compute_boltzman_distribution(dict_index_phi)
+        self.boltzmann_distribution = \
+            self.compute_boltzmann_distribution(dict_index_phi)
         self.write_msg(log)
 
-    def compute_boltzman_distribution(self,
+    def compute_boltzmann_distribution(self,
                                       dict_index_phi: dict[
                                           int, tuple[np.ndarray, ...]],
                                       ) -> dict[int, tuple[np.ndarray, ...]]:
         """Compute the Boltzman distribution"""
-        boltzman_dict: dict[int, tuple[np.ndarray, ...]] = {}
+        boltzmann_dict: dict[int, tuple[np.ndarray, ...]] = {}
         for i, phi_i_radii in dict_index_phi.items():
             if i in self.config.selected_grid:
                 dist = self.compute_distribution(phi_i_radii[0])
-                boltzman_dict[i] = (dist, phi_i_radii[1])
-        return boltzman_dict
+                boltzmann_dict[i] = (dist, phi_i_radii[1])
+        return boltzmann_dict
 
     def compute_distribution(self,
                              phi_i: np.float64
@@ -77,7 +77,7 @@ class ComputeBoltzmanDistribution:
         param: dict[str, float] = self.config.parameters
 
         # Calculate thermal energy k_B * T
-        kbt: float = param['T'] * param['k_boltzman_JK']
+        kbt: float = param['T'] * param['k_boltzmann_JK']
 
         co_eff: float = param['oda_concentration'] * 1e3 * \
             param['n_avogadro'] * param['e_charge']

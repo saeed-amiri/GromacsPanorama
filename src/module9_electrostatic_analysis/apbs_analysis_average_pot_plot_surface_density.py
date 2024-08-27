4266666e202577fe650a51b8d25829ed84be6616
Saeed Amiri <samiri@uni-bremen.de>
2024-08-27 15:04:15 +0200

    read and normalized the density

--- src/module9_electrostatic_analysis/apbs_analysis_average_pot_plot_surface_density.py
+++ src/module9_electrostatic_analysis/apbs_analysis_average_pot_plot_surface_density.py
@@ -10,6 +10,8 @@ in the [...]_plots.py file.
 """
 
 import typing
+from dataclasses import field
+from dataclasses import dataclass
 
 import numpy as np
 import pandas as pd
@@ -18,6 +20,79 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 from common import logger
+from common import xvg_to_dataframe
+from common.colors_text import TextColor as bcolors
 
 from module9_electrostatic_analysis.apbs_analysis_average_pot_plots import \
     plot_debye_surface_potential
+
+
+@dataclass
+class DensityFileConfig:
+    """setups for the input files names and their columns name"""
+    # pylint: disable=too-many-instance-attributes
+
+    normalize_type: str = 'max'  # or any other words neside max!
+
+    files: dict[str, dict[str, typing.Any]] = field(
+        default_factory=lambda: {
+            'dens_0': {'fname': 'SOL.xvg', 'y_col': 'SOL'},
+            'dens_1': {'fname': 'D10.xvg', 'y_col': 'D10'},
+            'dens_2': {'fname': 'ODN.xvg', 'y_col': 'ODN'},
+            'dens_3': {'fname': 'APT.xvg', 'y_col': 'APT'},
+            'dens_4': {'fname': 'CLA.xvg', 'y_col': 'CLA'},
+            'dens_5': {'fname': 'POT.xvg', 'y_col': 'POT'},
+            'dens_6': {'fname': 'COR.xvg', 'y_col': 'COR'},
+            'dens_7': {'fname': 'COR_APT.xvg', 'y_col': 'COR_APT'},
+            'dens_8': {'fname': 'ODN_N.xvg', 'y_col': 'NH2'},
+            })
+
+    plot_list: list[int] = \
+        field(default_factory=lambda: [0, 1, 7, 8])
+
+
+class DenityPlotConfiguration:
+    """The configuration for the density plot"""
+    # pylint: disable=missing-function-docstring
+    # pylint: disable=invalid-name
+    # pylint: disable=too-many-arguments
+
+
+class SurfacePotentialAndDensityPlot:
+    """
+    Plot the density of the system along the z-axis
+    """
+
+    info_msg: str = 'Message from SurfacePotentialAndDensityPlot:\n'
+    configs: DensityFileConfig
+
+    def __init__(self,
+                 log: logger.logging.Logger,
+                 configs: DensityFileConfig = DensityFileConfig()
+                 ) -> None:
+        self.file_configs = configs
+        self.plot_density(log)
+
+    def plot_density(self,
+                     log: logger.logging.Logger
+                     ) -> None:
+        """plot the density of the system"""
+        density_dict: dict[str, pd.DataFrame] = self.read_files(log)
+
+    def read_files(self,
+                   log: logger.logging.Logger
+                   ) -> dict[str, pd.DataFrame]:
+        """read the files and return the normalized density"""
+        density_dict: dict[str, pd.DataFrame] = {}
+        for i in self.file_configs.plot_list:
+            fname: str = self.file_configs.files[f'dens_{i}']['fname']
+            y_col: str = self.file_configs.files[f'dens_{i}']['y_col']
+            df: pd.DataFrame = xvg_to_dataframe.XvgParser(fname, log).xvg_df
+            denity_i: pd.DataFrame = df[[y_col]]
+            density_dict[y_col] = denity_i/np.max(denity_i)
+        return density_dict
+
+
+if __name__ == '__main__':
+    SurfacePotentialAndDensityPlot(
+        logger.setup_logger('plot_surface_density.log'))

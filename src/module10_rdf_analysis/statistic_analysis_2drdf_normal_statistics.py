4d5d9ad843fb156a12930d91b4b0f1b400261f48
Saeed Amiri <samiri@uni-bremen.de>
2024-11-14 09:47:00 +0100

    get medains and write them

--- src/module10_rdf_analysis/statistic_analysis_2drdf_normal_statistics.py
+++ src/module10_rdf_analysis/statistic_analysis_2drdf_normal_statistics.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 
 from common import logger
+from common import file_writer
 
 
 class NormalStatistics:
@@ -49,6 +50,9 @@ class NormalStatistics:
         """
         rdf_median: np.ndarray = self._calculate_median(self.data)
         fitted_median: np.ndarray = self._calculate_median(self.fit_data)
+        median_df: pd.DataFrame = \
+            self.arr_to_df(rdf_median, fitted_median)
+        self.write_median(median_df, log)
 
     @staticmethod
     def _calculate_median(data: pd.DataFrame,
@@ -66,3 +70,38 @@ class NormalStatistics:
             median_arr[idx, 0] = col
             median_arr[idx, 1] = median
         return median_arr
+
+    @staticmethod
+    def arr_to_df(rdf_median: np.ndarray,
+                  fitted_median: np.ndarray,
+                  ) -> pd.DataFrame:
+        """
+        Convert the array to a DataFrame
+        """
+        median_df: pd.DataFrame = pd.DataFrame(
+            columns=['oda', 'rdf_median', 'fitted_median'])
+        median_df['oda'] = rdf_median[:, 0]
+        median_df['rdf_median'] = rdf_median[:, 1]
+        median_df['fitted_median'] = fitted_median[:, 1]
+        # drop index and set the ODA as the index
+        median_df.set_index('oda', inplace=True)
+        return median_df
+
+    def write_median(self,
+                     median_df: pd.DataFrame,
+                     log: logger.logging.Logger
+                     ) -> None:
+        """
+        Write the median to a file
+        """
+        extra_comments: str = \
+            'median of the 2D RDF data, for normalized data and fitted data'
+        file_writer.write_xvg(
+            median_df,
+            log,
+            fname='median.xvg',
+            extra_comments=extra_comments,
+            xaxis_label='ODA',
+            yaxis_label='Median',
+            title='Median of the 2D RDF data'
+        )

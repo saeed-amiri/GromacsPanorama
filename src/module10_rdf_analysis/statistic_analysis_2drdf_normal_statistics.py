95e105f29df3ff15f7d36a3cb49ffb315d955da6
Saeed Amiri <samiri@uni-bremen.de>
2024-11-14 09:31:45 +0100

    compute the medain for both data

--- src/module10_rdf_analysis/statistic_analysis_2drdf_normal_statistics.py
+++ src/module10_rdf_analysis/statistic_analysis_2drdf_normal_statistics.py
@@ -39,7 +39,7 @@ class NormalStatistics:
         self.get_median(log)
 
     def get_median(self,
-                   logger: logger.logging.Logger
+                   log: logger.logging.Logger
                    ) -> None:
         """
         Get the median of the data
@@ -47,3 +47,22 @@ class NormalStatistics:
         middle value of a dataset when it is ordered from smallest to
         largest.
         """
+        rdf_median: np.ndarray = self._calculate_median(self.data)
+        fitted_median: np.ndarray = self._calculate_median(self.fit_data)
+
+    @staticmethod
+    def _calculate_median(data: pd.DataFrame,
+                          ) -> np.ndarray:
+        """
+        Get the median of the raw data
+        """
+        print('data shape:', len(data.columns))
+        median_arr: np.ndarray = np.zeros((len(data.columns), 2))
+        for idx, col in enumerate(data.columns):
+            rdf: np.ndarray = np.asanyarray(data[col])
+            rdf = np.sort(rdf)
+            rdf /= np.max(rdf)
+            median = np.median(rdf)
+            median_arr[idx, 0] = col
+            median_arr[idx, 1] = median
+        return median_arr

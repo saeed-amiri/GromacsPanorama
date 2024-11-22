139abd674f9877f19979cab9a72564457da22115
Saeed Amiri <samiri@uni-bremen.de>
2024-11-22 10:00:41 +0100

    set the plots` styles

--- src/module10_rdf_analysis/statistic_analysis_2drdf_boltzmann_plots.py
+++ src/module10_rdf_analysis/statistic_analysis_2drdf_boltzmann_plots.py
@@ -22,7 +22,7 @@ class PlotRdfBoltzmann:
     """
     # pylint: disable=too-many-arguments
 
-    __slots__ = ['config', 'info_msg']
+    __slots__ = ['config', 'info_msg', 'linestyles']
 
     def __init__(self,
                  rdf_x: pd.Series,
@@ -58,9 +58,15 @@ class PlotRdfBoltzmann:
         # last_ind: int = len(self.data.columns)
         boltzmann_x: pd.Series = boltzmann_data['r_nm']
         colors: list[str] = elsevier_plot_tools.CLEAR_COLOR_GRADIENT
-        linestyle: list[str] = [item[1] for item in
-                                elsevier_plot_tools.LINESTYLE_TUPLE][::-1]
+        self.linestyles = [
+            (offset, tuple(dashpattern)) if dashpattern else 'solid'
+            for (_, (offset, dashpattern)) in
+            elsevier_plot_tools.LINESTYLE_TUPLE][::-1]
+        # Validate linestyles
         markers: list[str] = elsevier_plot_tools.MARKER_STYLES
+        self.config.colors = colors
+        self.config.markers = markers
+        last_ind: int = len(rdf_data.columns)
         for i, (oda, rdf) in enumerate(rdf_data.items()):
             bolzmann: pd.Series = boltzmann_data[int(oda)]
             self._plot_axis(axes[i],
@@ -69,14 +75,19 @@ class PlotRdfBoltzmann:
                             boltzmann_x=boltzmann_x,
                             boltzmann=bolzmann,
                             color=colors[i],
-                            linestyle=linestyle[i],
+                            linestyle=self.linestyles[i],
                             marker=markers[i],
                             )
             self.add_vlines(axes[i], vlines_data[oda])
             oda_per_nm2: float = float(oda) / (21.7**2)
             self._add_label(axes[i], f'{oda_per_nm2: .2f} ODA/nm$^2$')
+
+        self._plot_all_rdf(rdf_data, axes[last_ind], rdf_x, 'rdf')
+        self._plot_all_rdf(
+            boltzmann_data, axes[last_ind + 1], boltzmann_x, 'boltzmann')
         self._set_or_remove_ticks(axes)
         self._add_grid(axes)
+        self._add_axis_labels(axes)
         self._save_figure(fig_i)
 
     def _make_axis(self) -> tuple[plt.Figure, np.ndarray]:
@@ -139,6 +150,34 @@ class PlotRdfBoltzmann:
                      label=self.config.r_half_max_label,
                      )
 
+    def _plot_all_rdf(self,
+                      rdf_data: pd.DataFrame,
+                      ax_i: mp.axes._axes.Axes,
+                      x_data: pd.Series,
+                      style: str = 'boltzmann',
+                      ) -> None:
+        """plot the fitted rdf"""
+        for i, (_, rdf) in enumerate(rdf_data.items()):
+            if style == 'boltzmann':
+                if i == 0:
+                    continue
+                marker = ''
+                linestyle = self.linestyles[i]
+                lw: float = 1.0
+            else:
+                marker = self.config.markers[i]
+                linestyle = '--'
+                lw = 0.5
+            ax_i.plot(x_data,
+                      rdf,
+                      markersize=0.5,
+                      color=self.config.colors[i],
+                      linestyle=linestyle,
+                      lw=lw,
+                      marker=marker,
+                      )
+        ax_i.set_yticklabels([])
+
     def _add_label(self,
                    ax_i: mp.axes._axes.Axes,
                    label: str,
@@ -173,6 +212,20 @@ class PlotRdfBoltzmann:
         """add the grid"""
         for ax_i in axes:
             ax_i.grid(True, which='both', linestyle='--', lw=0.5)
+
+    def _add_axis_labels(self,
+                         axes: np.ndarray  # of plt.Axes
+                         ) -> None:
+        """add the axis labels"""
+        for ind, ax_i in enumerate(axes):
+            if ind % self.config.nr_rows == 0:
+                ax_i.set_ylabel(
+                    self.config.ylabel,
+                    fontsize=elsevier_plot_tools.LABEL_FONT_SIZE_PT)
+            if ind >= (self.config.nr_cols - 1) * self.config.nr_rows:
+                ax_i.set_xlabel(
+                    self.config.xlabel,
+                    fontsize=elsevier_plot_tools.LABEL_FONT_SIZE_PT)
 
     def _save_figure(self,
                      fig_i: plt.Figure,

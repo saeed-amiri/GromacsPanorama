2cce9f2ab94d876c5d5d132a3487be28ef4db6fa
Saeed Amiri <samiri@uni-bremen.de>
2024-12-17 10:44:27 +0100

    write the atoms in different tabels

--- src/module13_backing_up_data/forcefield_to_latex_write_tex.py
+++ src/module13_backing_up_data/forcefield_to_latex_write_tex.py
@@ -45,21 +45,52 @@ class WriteTex:
                        ) -> None:
         """Writes the atoms to a LaTeX file."""
         residues: list[str] = list(atoms_df['resname'].unique())
-        atoms_lines: list[str] = []
+        residues = [residue.upper() for residue in residues]
+
+        residue_table_groups: list[tuple[str, ...]] = \
+            self.cfg.structures.structures['residue_table_groups']
+        fname: str = self.cfg.files.latex_path['atoms']
+        with open(fname, 'w', encoding='utf-8') as file:
+            for group in residue_table_groups:
+                print(f'group: {group}')
+                atoms_lines: list[str] = self._make_atoms_lines(
+                    group, residues, atoms_df)
+                content: str = ''.join(
+                    '\n'.join(self.cfg.files.latex_headers['atoms_header']) +
+                    ''.join(atoms_lines) +
+                    '\n'.join(self.cfg.files.latex_headers['atoms_footer']) +
+                    '\n\n'
+                )
+                file.write(content)
+                del content
+        log.info(f'\tWrote the atoms to {fname}\n')
+
+    def _make_atoms_lines(self,
+                          group: tuple[str, ...],
+                          residues: list[str],
+                          atoms_df: pd.DataFrame,
+                          ) -> list[str]:
+        """Makes the lines for the atoms table."""
         structure_list: list[str] = \
             self.cfg.structures.structures['structure_list']
-        for residue in residues:
+        atoms_lines: list[str] = []
+        for residue in group:
+            if residue not in residues:
+                print(f'{residue} not in the residues')
+                continue
             residue_name: str = \
-                self.cfg.files.residue_names.get(residue.upper(), residue)
+                self.cfg.files.residue_names.get(residue, residue)
             atoms_lines.append(f'\\textbf{{{residue_name}}} & & & \\\\\n')
-            df: pd.DataFrame = atoms_df[atoms_df['resname'] == residue]
+            df: pd.DataFrame = atoms_df[atoms_df['resname'] == residue.lower()]
             # sort the df by the atomtypes
             df = df.sort_values(by=['element', 'atomtype'])
-            if residue.upper() in structure_list:
-                structure: str = self.cfg.structures.structures[residue.upper()]
-                atoms_lines.append('\\multicolumn{4}{c}{{}}\\\ \n')
+            print(f'df: {df}')
+            if residue in structure_list:
+                structure: str = \
+                    self.cfg.structures.structures[residue]
+                atoms_lines.append('\\multicolumn{4}{c}{{}}\\\\\n')
                 atoms_lines.append(
-                    f"\\multicolumn{{4}}{{c}}{{{structure}}}\\\ \n")
+                    f"\\multicolumn{{4}}{{c}}{{{structure}}}\\\\\n")
             else:
                 pass
             for _, row in df.iterrows():
@@ -72,16 +103,7 @@ class WriteTex:
                 )
                 atoms_lines.append(l_line)
             atoms_lines.append(' & & & \\\\\n')
-
-        fname: str = self.cfg.files.latex_path['atoms']
-        with open(fname, 'w', encoding='utf-8') as file:
-            content: str = ''.join(
-                '\n'.join(self.cfg.files.latex_headers['atoms_header']) +
-                ''.join(atoms_lines) +
-                '\n'.join(self.cfg.files.latex_headers['atoms_footer'])
-            )
-            file.write(content)
-        log.info(f'\tWrote the atoms to {fname}\n')
+        return atoms_lines
 
     def bonds_to_latex(self,
                        bonds_df: pd.DataFrame,
